90add174ff16d0c00eb53ad4c78da4fcac778022
Wang Qing <vincent.w.qing@gmail.com>
2022-03-23 23:29:46 -0400

    updating heuristics

--- HeuDiConv/Heuristics_PPMI_all.py
+++ HeuDiConv/Heuristics_PPMI_all.py
@@ -1,5 +1,5 @@
 import os
-# Heuristics created by Vincent for PPMI dataset T1 images.
+# Heuristics created by Vincent for PPMI dataset T1/T2/DTI images.
 # 22th Mar. 2022
 
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
@@ -16,14 +16,15 @@ def infotodict(seqinfo):
     subindex: sub index within group
     """
     t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_run-{item:01d}_T1w')
-    #t2w = create_key('sub-{subject}/{session}/anat/sub-{subject}_run-{item:01d}_T2w')
-    #dwi = create_key('sub-{subject}/{session}/dwi/sub-{subject}_run-{item:01d}_dwi')
+    t2w = create_key('sub-{subject}/{session}/anat/sub-{subject}_run-{item:01d}_T2w')
+    dwi = create_key('sub-{subject}/{session}/dwi/sub-{subject}_run-{item:01d}_dwi')
     #dwi_fieldmap = create_key('sub-{subject}/{session}/fmap/sub-{subject}_acq-dwi_run-{item:01d}_fieldmap')
     #rest = create_key('sub-{subject}/{session}/func/sub-{subject}_task-rest_run-{item:01d}_bold')
     #rest_fieldmap = create_key('sub-{subject}/{session}/fmap/sub-{subject}_task-rest_acq-bold_run-{item:01d}_fieldmap')
     #swi = create_key('sub-{subject}/{session}/swi/sub-{subject}_run-{item:01d}_swi')
     #info = {t1w: [], t2w: [], dwi: [], rest: [],  dwi_fieldmap: [], rest_fieldmap: [], swi: []}
     info = {t1w: []}
+    
     data = create_key('run{item:03d}')
     last_run = len(seqinfo)
     for idx, s in enumerate(seqinfo):
@@ -32,16 +33,12 @@ def infotodict(seqinfo):
             if ('MPRAGE' in s.series_description) or ('T1' in s.series_description):
                 print("****** T1 ******")
                 info[t1w].append(s.series_id)
-        #if ('T2 Axial' in s.protocol_name):
-        #    print("****** T2 ******")
-        #    info[t2w].append(s.series_id) 
-        #if ('diffusion' in s.protocol_name) or ('DIFFUSION' in s.protocol_name):
-        #    if 'gre_field_mapping' in s.protocol_name:
-        #        print("****** DWI filedmap ******")
-        #        info[dwi_fieldmap].append(s.series_id)
-        #    else:
-        #        print("****** DWI ******")
-        #        info[dwi].append(s.series_id)
+            if 'T2' in s.series_description:
+                print("****** T2 ******")
+                info[t2w].append(s.series_id)
+            if 'DTI' in s.series_description:
+                print("****** DWI ******")
+                info[dwi].append(s.series_id)
         #print(s.is_motion_corrected)
         #print('BOLD: ', 'BOLD' in s.protocol_name)
         #print('bold: ', 'bold' in s.protocol_name)
