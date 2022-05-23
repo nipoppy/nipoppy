f6b2c2168d51f4f9575784b03ce11a44d023b879
Wang Qing <vincent.w.qing@gmail.com>
2022-05-23 16:15:43 -0400

    update folder handling for fmriprep.

--- fMRIPrep/fmriprep_anat_sub.sh
+++ fMRIPrep/fmriprep_anat_sub.sh
@@ -3,12 +3,13 @@ if [ "$#" -ne 4 ]; then
 DATA_NAME=(${@:1:1})
 CLEAN_RUN_FLAG=(${@:2:1})
 RUN_LIST_NAME=(${@:3:1})
-echo "Cleaning old results..."
+RUNNING_TAB="Y"
 else
 DATA_NAME=(${@:1:1})
 CLEAN_RUN_FLAG=(${@:2:1})
 SUB_ID=(${@:3:1})
 SES_ID=(${@:4:1})
+RUNNING_TAB="N"
 echo 'Rerunning subj: ' ${SUB_ID} ', ses' ${SES_ID}
 fi
 echo ${DATA_NAME}
@@ -39,27 +40,33 @@ else
 	python -c "from templateflow import api; api.get('OASIS30ANTs')"
 fi
 
-# creating folders and envs
+# Folder cleaning
 if [ ${CLEAN_RUN_FLAG} == 'Y' ];then
+
+
 # create session outputs
-#RUN_LIST="preproc/runningMDD_rerun.csv" # for easy test
 for SES_ in $(sed -n '/sub/s/^.*,\([0-9]*\).*$/\1/p' ${RUN_LIST} | sort -u)
 do
 echo "creating folders for session "$SES_
 OUT_DIR=${DATA_DIR}_ses-${SES_}_fmriprep_anat_${FMRIPREP_VER}
 if [ -d ${OUT_DIR} ];then
+echo "cleaning folder for session "$SES_
 rm -rf ${OUT_DIR}
 fi
+echo "creating new folder for session "$SES_
 mkdir -p ${OUT_DIR}
 done
+
+# clearing logs
 LOG_DIR=${DATA_DIR}_fmriprep_anat_log
 if [ -d ${LOG_DIR} ];then
   rm -rf ${LOG_DIR}
 fi
 mkdir -p ${LOG_DIR}
+
 fi
 
-if [ ${CLEAN_RUN_FLAG} == 'Y' ];then
+if [ ${RUNNING_TAB} == 'Y' ];then
 while read line; do
     # Do what you want to $name
     SUB_ID_STR="$(cut -d',' -f1 <<<${line})"
