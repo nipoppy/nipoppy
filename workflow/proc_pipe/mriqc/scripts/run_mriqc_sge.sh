35187546cbf704dd066a8f646e52b83b4906ef2f
InesGP <g_grrrls@yahoo.ca>
2022-10-11 13:55:38 -0400

    generalized run script

--- workflow/proc_pipe/mriqc/scripts/run_mriqc_sge.sh
+++ workflow/proc_pipe/mriqc/scripts/run_mriqc_sge.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #
 #$ -cwd
-#$ -e /home/bic/inesgp/mriqc_qpn_err.log
+#$ -e ERROR LOG FILE 
 #$ -l h_vmem=60G
-#$ -M inesgp99@gmail.com
+#$ -M EMAIL ADDRESS
 #$ -m bae
 #$ -q origami.q
 #$ -t 120-162
 
-#umask u=rwx,g=rwx,o=rx
-#echo `which mkdir`
-
-while getopts i:r:p: flag
+while getopts d:r:p:c: flag
 do
     case "${flag}" in
+        c) container=${OPTARG};;
         d) data_dir=${OPTARG};;
         r) results_dir=${OPTARG};;
         p) subject_list=${OPTARG};;
@@ -22,7 +20,10 @@ done
 
 index=$((${SGE_TASK_ID}+1))
 subject_id=`sed -n "${index}p" $subject_list`
+#new lines to extract subject from participant tsv file
+subject_id=(${subject_id// / })
+subject_id=${subject_id[0]}
 echo $subject_id >> $results_dir/mriqc_out_$SGE_TASK_ID.log
 
-singularity run --cleanenv -B ${data_dir}:/data:ro -B ${results_dir}:/out /data/pd/qpn/proc/containers/mriqc_patch2.simg --no-sub /data /out participant --participant-label $subject_id >> ${results_dir}/mriqc_out_$SGE_TASK_ID.log
+singularity run --cleanenv -B ${data_dir}:/data:ro -B ${results_dir}:/out $container --no-sub /data /out participant --participant-label $subject_id >> ${results_dir}/mriqc_out_$SGE_TASK_ID.log
 
