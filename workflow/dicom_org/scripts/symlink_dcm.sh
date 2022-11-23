840db148016dd7c324e4776b55ea4aba858d9f39
nikhil153 <nikhil153@gmail.com>
2022-11-23 15:27:39 -0500

    fixed BIC paths

--- workflow/dicom_org/scripts/symlink_dcm.sh
+++ workflow/dicom_org/scripts/symlink_dcm.sh
@@ -8,27 +8,27 @@
 # date: 12 April 2022
 
 if [ "$#" -ne 3 ]; then
-  echo "Please provide DICOM_SOURCE_DIR, mr_proc_dataset_root_dir, and the subject_list_file"
+  echo "Please provide DICOM_SOURCE_DIR, DICOM_DEST_DIR, and the subject_list_file"
   exit 1
 fi
 
 DICOM_SOURCE_DIR=$1
-DATASET_ROOT=$2
+DICOM_DEST_DIR=$2
 SUBJECT_LIST=$3
-
-MR_PROC_DICOM_DIR="$DATASET_ROOT/dicom"
 
 N_SUBS=`cat $SUBJECT_LIST | wc -l`
 echo "Number of subjects in the batch: $N_SUBS"
+echo ""
+echo "Symlinking dicom dirs from $DICOM_SOURCE_DIR to $DICOM_DEST_DIR"
+echo ""
 
-echo "Symlinking dicom dirs from $DICOM_SOURCE_DIR to $MR_PROC_DICOM_DIR"
 for sub in `cat $SUBJECT_LIST`; do 
-   i=`ls $sub`
+   i=`ls ${DICOM_SOURCE_DIR} | grep ${sub}`
    PSCID=`echo $i | cut -d "_" -f1`
    DCCID=`echo $i | cut -d "_" -f2`
    BIDS_ID="${PSCID}D${DCCID}"
-
-   ln -s ${DICOM_SOURCE_DIR}/${i} $MR_PROC_DICOM_DIR/${BIDS_ID}
+   echo "subject_id: $sub, dicom_file: $i, bids_id: $BIDS_ID"
+   ln -s ${DICOM_SOURCE_DIR}/${i} $DICOM_DEST_DIR/${BIDS_ID}
 done
-
+echo ""
 echo "Symlinking complete"
