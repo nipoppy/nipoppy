850a38ba84a8f50684a3ee0643acf8dc3f9051e0
nikhil153 <nikhil153@gmail.com>
2022-11-23 21:01:10 -0500

    added DICOM selection for multiple matches

--- workflow/dicom_org/scripts/fetch_new_scans.sh
+++ workflow/dicom_org/scripts/fetch_new_scans.sh
@@ -34,13 +34,20 @@ for i in `cat $SUBJECT_LIST`; do
       echo $i >> $MISSING_SUBJECT_LIST
    else
       find_mri -claim -noconfir $DICOM_NAME 
-      cp -r ${DICOM_NAME} ${DATASET_DICOM_DIR}/
-      chmod -R 775 ${DATASET_DICOM_DIR}/${i}*
-
+      for k in `echo $DICOM_NAME | tr ' ' '\n'`; do
+         if [ ! -d ${DATASET_DICOM_DIR}/${k} ]; then
+            cp -r ${k} ${DATASET_DICOM_DIR}/
+            chmod -R 775 ${DATASET_DICOM_DIR}/${i}*
+         else
+            echo "${DATASET_DICOM_DIR}/${k} exists" 
+         fi
+      done
       for j in ${DATASET_DICOM_DIR}/${i}*; do
          if tar tf "$j" 2> /dev/null 1>&2; then 
             echo "untarring $j"
+            k=`echo $j | cut -d "." -f1`
             tar xzf $j
+            mv $j ${DATASET_DICOM_DIR}/$k
             rm -rf $j
          fi
       done
