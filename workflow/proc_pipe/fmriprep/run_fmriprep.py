9c036e2990b441dc4d74d0f9f7fe84aadb611397
nikhil153 <nikhil153@gmail.com>
2023-02-10 20:05:48 -0500

    major cleanup for main branch

--- workflow/proc_pipe/fmriprep/run_fmriprep.py
+++ workflow/proc_pipe/fmriprep/run_fmriprep.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import shutil
 
 #Author: nikhil153
-#Date: 07-Oct-2022
+#Date: 05-Feb-2023 (last update)
 
 # argparse
 HELPTEXT = """
@@ -25,28 +25,32 @@ parser.add_argument('--test_run', action='store_true', help='do a test run or no
 
 args = parser.parse_args()
 
-global_config = args.global_config
+global_config_file = args.global_config
 participant_id = args.participant_id
 session_id = args.session_id
 output_dir = args.output_dir # Needed on BIC (QPN) due to weird permissions issues with mkdir
 use_bids_filter = args.use_bids_filter
 bids_filter = str(int(use_bids_filter)) #reformat for shell script argument
 anat_only = str(int(args.anat_only))
-test_run = str(int(args.test_run))
+test_run = args.test_run
 
   
 # Read global configs
-f = open(global_config)
-json_data = json.load(f)
+with open(global_config_file, 'r') as f:
+    global_configs = json.load(f)
 
-DATASET_ROOT = json_data["DATASET_ROOT"]
-SINGULARITY_PATH = json_data["SINGULARITY_PATH"]
-TEMPLATEFLOW_DIR = json_data["TEMPLATEFLOW_DIR"]
+DATASET_ROOT = global_configs["DATASET_ROOT"]
+DATASTORE_DIR = global_configs["DATASTORE_DIR"]
+TEMPLATEFLOW_DIR = global_configs["TEMPLATEFLOW_DIR"]
+SINGULARITY_PATH = global_configs["SINGULARITY_PATH"]
+CONTAINER_STORE = global_configs["CONTAINER_STORE"]
 
-FMRIPREP_VERSION = json_data["FMRIPREP_VERSION"]
+FMRIPREP_CONTAINER = global_configs["PROC_PIPELINES"]["fmriprep"]["CONTAINER"]
+FMRIPREP_VERSION = global_configs["PROC_PIPELINES"]["fmriprep"]["VERSION"]
+FS_VERSION = global_configs["PROC_PIPELINES"]["freesurfer"]["VERSION"]
+FMRIPREP_CONTAINER = FMRIPREP_CONTAINER.format(FMRIPREP_VERSION)
 
-SINGULARITY_FMRIPREP = json_data["SINGULARITY_FMRIPREP"]
-SINGULARITY_FMRIPREP = SINGULARITY_FMRIPREP.format(FMRIPREP_VERSION)
+SINGULARITY_FMRIPREP = f"{CONTAINER_STORE}{FMRIPREP_CONTAINER}"
 
 if output_dir is None:
     output_dir = DATASET_ROOT
@@ -55,29 +59,37 @@ print(f"Using DATASET_ROOT: {DATASET_ROOT}")
 print(f"Using output_dir: {output_dir}")
 print(f"Using SINGULARITY_FMRIPREP: {SINGULARITY_FMRIPREP}")
 
-# Create version specific output dir
-Path(f"{output_dir}/derivatives/fmriprep/v{FMRIPREP_VERSION}").mkdir(parents=True, exist_ok=True)
+# Specify paths
+if test_run:
+    print("Doing a test run")
+    bids_dir = f"{DATASET_ROOT}/test_data/bids/"
+    derivs_dir = f"{output_dir}/test_data/derivatives/"
+else:
+    bids_dir = f"{DATASET_ROOT}/bids/"
+    derivs_dir = f"{output_dir}/derivatives/"
+
+fmriprep_dir = f"{output_dir}/derivatives/fmriprep/v{FMRIPREP_VERSION}"
+# freesurfer won't
+FS_dir = f"{output_dir}/derivatives/freesurfer/v{FS_VERSION}/output/ses-{session_id}"
+FS_license = f"{output_dir}/derivatives/freesurfer/license.txt"
+
+Path(f"{fmriprep_dir}/output").mkdir(parents=True, exist_ok=True)
+Path(FS_dir).mkdir(parents=True, exist_ok=True)
 
 fname = __file__
 CWD = os.path.dirname(os.path.abspath(fname))
-print(f"CWD: {CWD}, fname: {fname}")
+# print(f"CWD: {CWD}, fname: {fname}")
 
-#Copy bids_filter.json `<DATASET_ROOT>/bids/bids_filter.json`
+# Copy bids_filter.json `<DATASET_ROOT>/bids/bids_filter.json`
 if use_bids_filter:
     print(f"Copying ./bids_filter.json to {DATASET_ROOT}/bids/bids_filter.json (to be seen by Singularity container)")
-    if test_run == "1":
-        shutil.copyfile(f"{CWD}/bids_filter.json", f"{DATASET_ROOT}/test_data/bids/bids_filter.json")
-    else:
-        shutil.copyfile(f"{CWD}/bids_filter.json", f"{DATASET_ROOT}/bids/bids_filter.json")
+    shutil.copyfile(f"{CWD}/bids_filter.json", f"{bids_dir}/bids_filter.json")
 
-# Run FMRIPREP script
-# "Sample cmd: ./run_fmriprep_anat_and_func.sh -d <dataset_root> -i <path_to_fmriprep_img> -r <singularity> \
-#         -f <path_to_templateflow_dir> -p <MNI01> -s <01> -b 1 -a 1 -t 1"
-
+# setup singularity run script command
 FMRIPREP_SCRIPT = f"{CWD}/scripts/run_fmriprep.sh"
-FMRIPREP_ARGS = ["-d", DATASET_ROOT, "-o", output_dir, "-i", SINGULARITY_FMRIPREP, "-r", SINGULARITY_PATH, \
-                 "-f", TEMPLATEFLOW_DIR, "-p", participant_id, "-s", session_id, "-b", bids_filter, \
-                 "-a", anat_only, "-v", f"v{FMRIPREP_VERSION}", "-t", test_run]
+FMRIPREP_ARGS = ["-d", bids_dir, "-o", fmriprep_dir, "-f", FS_dir, "-l", FS_license, "-p", participant_id, \
+                 "-c", SINGULARITY_FMRIPREP, "-r", SINGULARITY_PATH, "-t", TEMPLATEFLOW_DIR, \
+                 "-b", bids_filter, "-a", anat_only]
 FMRIPREP_CMD = [FMRIPREP_SCRIPT] + FMRIPREP_ARGS
 
 fmriprep_proc = subprocess.run(FMRIPREP_CMD)
