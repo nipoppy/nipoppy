ce046583e7283fdf4641a9cc070037ffbec45773
InesGP <g_grrrls@yahoo.ca>
2023-02-16 12:15:23 -0500

    modified script to follow tracker schema

--- workflow/proc_pipe/mriqc/eval_mriqc_results.py
+++ workflow/proc_pipe/mriqc/eval_mriqc_results.py
@@ -4,80 +4,56 @@ import glob
 import pandas as pd
 import logging
 
+PIPELINE_NAME = 'MRIQC'
+PIPELINE_VERSION = '22.0.1'
+PIPELINE_STARTTIME = '2023-01-23 00:00:00'
 
-def eval_mriqc(args): 
 
+def eval_mriqc(args): #
+    
     #load config file
     config = json.load(open(args[1])) 
-    participants = pd.read_csv(config['subject_list'], sep='\t')
-    #subject_id = participants['participant_id'][int(args[-1])+1]
+    
+    #check if there's additional arguments
+    if 'pipeline_name' in config.keys():
+        PIPELINE_NAME = config['pipeline_name']
+    if 'pipeline_version' in config.keys():
+        PIPELINE_VERSION = config['pipeline_version']
+    if 'pipeline_starttime' in config.keys():
+        PIPELINE_STARTTIME = config['pipeline_starttime']
+
     
     logging.basicConfig(filename='%s/mriqc_eval_err.log'%config['results_dir'], level=logging.DEBUG)
-   
-    for index in range(len(participants)):
-    
-    	#get subject ID
-    	subject_id = participants['participant_id'][index]
-    	
-        #read MRIQC pipeline output log
-        output_log = config['input_dir'] + '/mriqc_out_' + str(subject_id.split('-')[-1]) + '.log'
-        f = open(output_log, 'r')
+
+    #read MRIQC pipeline output log
+    output_log = config['input_dir'] + '/mriqc_out_' + str(config['subject_id']) + '.log'
+    f = open(output_log, 'r')
 
 
-        logging.info(subject_id)
+    logging.info(config['subject_id'])
 
-        results = {'participant_id': [subject_id], 'session_id': [config['session_id']], 'T1w': [], 'BOLD': []} #default datatypes
-        results.update({a: [] for a in config['file_names']})
+    results = {'participant_id': [config['subject_id']], 'session': [config['session_id']], 
+               'pipeline_name': [PIPELINE_NAME], 'pipeline_version': [PIPELINE_VERSION], 
+               'pipeline_starttime': [PIPELINE_STARTTIME], 'PIPELINE_STATUS_COLUMNS': [], 'MRIQC_BOLD': []} 
     
         
-        #check if participant successfully passed MRIQC pipeline
-        if "Participant level finished successfully." in f.read(): 
-    
-            for arg in results:
-            
-                acq2 = None
-                if arg == 'participant_id' or arg == 'session_id': continue
-                
-                elif arg == 'T1w': #default check
-                    acq1 = config['input_dir'] + '/' + subject_id + '_ses-' + config['session_id'] + '*_run-*_T1w*'
-            
-                elif arg == 'BOLD': #default check 
-                    acq1 = config['input_dir'] + '/' + subject_id + '_ses-' + config['session_id'] + '*_task-rest_run-*_bold*'
-                
-                else: #check for unknown datatypes
-                    acq1 = config['input_dir'] + '/' + subject_id + '_ses-' + config['session_id'] + '*_' + arg + '_run-*_T1w*'
-                    acq2 = config['input_dir'] + '/' + subject_id + '_ses-' + config['session_id'] + '*_' + arg + '_run-*_T2w*'
-
-                if glob.glob(acq1):
-                    results[arg].append('Success')
-                else: 
-                    if acq2:
-                        if glob.glob(acq2):
-                            results[arg].append('Success')
-                        else: results[arg].append('Fail')
-                    
-                    else: results[arg].append('Fail')
-                
-        else: #no sign participant passed, assume failure for all datatypes
-            for a in results: 
-                if a == 'participant_id' or a == 'session_id': continue
-                results[a].append('Fail')
-    
-        logging.info(results)
+    #check if participant successfully passed MRIQC pipeline
+    if "Participant level finished successfully." in f.read(): 
         
-        #create/append to csv file
-        df = pd.DataFrame(results)
-        first_column = df.pop('participant_id')
-        second_column = df.pop('session_id')
-        df.insert(0, 'participant_id', first_column)
-        df.insert(1, 'session_id', second_column)
+        acq_T1w = config['input_dir'] + '/' + config['subject_id'] + '_ses-' + config['session_id'] + '*_run-*_T1w*'
+        acq_bold = config['input_dir'] + '/' + config['subject_id'] + '_ses-' + config['session_id'] + '*_task-rest_run-*_bold*'
+        
+        if glob.glob(acq_T1w):
+                results['PIPELINE_STATUS_COLUMNS'].append('SUCCESS')
+        else: results['PIPELINE_STATUS_COLUMNS'].append('FAIL')
+            
+        if glob.glob(acq_bold):
+                results['MRIQC_BOLD'].append('SUCCESS')
+        else: results['MRIQC_BOLD'].append('FAIL')
 
-
-        results_file = config['results_dir'] + '/' + config['csv_name'] + '.csv'
-        if glob.glob(results_file):
-
-            df.to_csv(results_file, mode='a', index=False, header=False)
-        else: df.to_csv(results_file, index=False)
-
-eval_mriqc(sys.argv)
-
+    
+    else: #no sign participant passed, assume failure for all datatypes
+        results['PIPELINE_STATUS_COLUMNS'].append('FAIL')
+        results['MRIQC_BOLD'].append('FAIL')
+        
+    return results
