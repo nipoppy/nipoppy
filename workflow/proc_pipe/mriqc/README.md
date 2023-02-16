c40d089a30637cdfdc98db55db7a5e32dd76bd72
InesGP <g_grrrls@yahoo.ca>
2023-02-16 16:55:10 -0500

    modified mriqc input/output

--- workflow/proc_pipe/mriqc/README.md
+++ workflow/proc_pipe/mriqc/README.md
@@ -18,8 +18,16 @@
 
 ### 1.2 Evaluate MRIQC Results
 - Use [mriqc_tracker.py](https://github.com/InesGP/mr_proc/blob/main/workflow/proc_pipe/mriqc/eval_mriqc_results.py) to determine how many subjects successfully passed through the MRIQC pipeline
-	- Mandatory: Pass in a [JSON configuration file](https://github.com/InesGP/mr_proc/blob/main/workflow/proc_pipe/mriqc/mriqc_config.json) that contains the input directory, the results directory, the subject ID and the desired session ID
+	- Mandatory: Pass in the subject directory as an argument
 	- Multiple sessions can be evaluated, but each session will require a new job running this script
 - Example command:
-	- python mriqc_tracker.py mriqc_config.json
+	``` 
+	from mriqc_tracker import tracker_configs 
+	
+	results = {}
+	for name, func in tracer_configs.items():
+		if type(func) == dict:
+			results[name] = {"MRIQC_BOLD': func['MRIQC_BOLD'](subject_dir)}
+		else: results[name] = func(subject_dir)
+	```
 - After a successful run of the script, a dictionary called `tracker_configs` is returned contained whether the subject passed through the pipeline successfully
--- workflow/trackers/mriqc_tracker.py
+++ workflow/trackers/mriqc_tracker.py
@@ -2,40 +2,25 @@ import sys
 import json
 import glob
 
-PIPELINE_NAME = 'MRIQC'
-PIPELINE_VERSION = '22.0.1'
-PIPELINE_STARTTIME = '2023-01-23 00:00:00'
-
-
-def eval_mriqc(args):
+def eval_mriqc(subject_dir):
     
-    #load config file
-    config = json.load(open(args[1])) 
-    
-    #check if there's additional arguments
-    if 'pipeline_name' in config.keys():
-        PIPELINE_NAME = config['pipeline_name']
-    if 'pipeline_version' in config.keys():
-        PIPELINE_VERSION = config['pipeline_version']
-    if 'pipeline_starttime' in config.keys():
-        PIPELINE_STARTTIME = config['pipeline_starttime']
+       
+    subject_id = subject_dir.split('/')[-1].split('-')[-1]
+    session_id = [x for x in subject_dir.split('/') if 'ses' in x][0].split('-')[-1]
 
-    
 
     #read MRIQC pipeline output log
-    output_log = config['input_dir'] + '/mriqc_out_' + str(config['subject_id']) + '.log'
+    output_log = subject_dir + '/mriqc_out_' + str(subject_id) + '.log'
     f = open(output_log, 'r')
 
 
-    results = {'participant_id': [config['subject_id']], 'session': [config['session_id']], 
-               'pipeline_name': [PIPELINE_NAME], 'pipeline_version': [PIPELINE_VERSION], 
-               'pipeline_starttime': [PIPELINE_STARTTIME], 'PIPELINE_STATUS_COLUMNS': []} 
+    results = {'participant_id': [subject_id], 'session': [session_id], 'PIPELINE_STATUS_COLUMNS': []} 
     
         
     #check if participant successfully passed MRIQC pipeline
     if "Participant level finished successfully." in f.read(): 
         
-        acq_T1w = config['input_dir'] + '/' + config['subject_id'] + '_ses-' + config['session_id'] + '*_run-*_T1w*'
+        acq_T1w = subject_dir + '/' + str(subject_id) + '_ses-' + str(session_id) + '*_run-*_T1w*'
         
         if glob.glob(acq_T1w):
                 results['PIPELINE_STATUS_COLUMNS'].append('SUCCESS')
@@ -48,18 +33,19 @@ def eval_mriqc(args):
     return results
 
 
-def check_bold(args):
+def check_bold(subject_dir):
     
-    #load config file
-    config = json.load(open(args[1])) 
+    
+    subject_id = subject_dir.split('/')[-1].split('-')[-1]
+    session_id = [x for x in subject_dir.split('/') if 'ses' in x][0].split('-')[-1]
     
     #read MRIQC pipeline output log
-    output_log = config['input_dir'] + '/mriqc_out_' + str(config['subject_id']) + '.log'
+    output_log = subject_dir + '/mriqc_out_' + str(subject_id) + '.log'
 
     f = open(output_log, 'r')
     
     if "Participant level finished successfully." in f.read(): 
-        acq_bold = config['input_dir'] + '/' + config['subject_id'] + '_ses-' + config['session_id'] + '*_task-rest_run-*_bold*'
+        acq_bold = subject_dir + '/' + subject_id + '_ses-' + session_id + '*_task-rest_run-*_bold*'
     
         if glob.glob(acq_bold):
                 return 'SUCCESS'
@@ -70,11 +56,10 @@ def check_bold(args):
          
 
 tracker_configs = {
-    "pipeline_complete": eval_mriqc(sys.argv),
+    "pipeline_complete": eval_mriqc,
     
     "Stage_": {
-            "MRIQC_BOLD": check_bold(sys.argv)
+            "MRIQC_BOLD": check_bold
             }
 }
-
 
