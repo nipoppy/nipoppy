62497a4473788cb45a2f8e2ed8a7a486dcb9ade5
InesGP <g_grrrls@yahoo.ca>
2023-03-01 08:33:00 -0500

    update mriqc tracker

--- workflow/trackers/mriqc_tracker.py
+++ workflow/trackers/mriqc_tracker.py
@@ -2,26 +2,31 @@ import sys
 import json
 import glob
 
-def eval_mriqc(subject_dir):
+
+#/scratch/qpn/sub-01/
+
+def eval_mriqc(subject_dir, session_id):
     
        
+    #get subject id from directory
     subject_id = subject_dir.split('/')[-1].split('-')[-1]
-    session_id = [x for x in subject_dir.split('/') if 'ses' in x][0].split('-')[-1]
 
 
     #read MRIQC pipeline output log
     output_log = subject_dir + '/mriqc_out_' + str(subject_id) + '.log'
     f = open(output_log, 'r')
 
-
-    results = {'participant_id': [subject_id], 'session': [session_id], 'PIPELINE_STATUS_COLUMNS': []} 
+    #set up dictionary to return
+    results = {'participant_id': [subject_id], 'PIPELINE_STATUS_COLUMNS': []} 
     
         
     #check if participant successfully passed MRIQC pipeline
     if "Participant level finished successfully." in f.read(): 
         
+        #create path string
         acq_T1w = subject_dir + '/' + str(subject_id) + '_ses-' + str(session_id) + '*_run-*_T1w*'
         
+        #check if string is an existing path
         if glob.glob(acq_T1w):
                 results['PIPELINE_STATUS_COLUMNS'].append('SUCCESS')
         else: results['PIPELINE_STATUS_COLUMNS'].append('FAIL')
@@ -33,20 +38,24 @@ def eval_mriqc(subject_dir):
     return results
 
 
-def check_bold(subject_dir):
+def check_bold(subject_dir, session_id):
     
-    
+    #get subject id from directory
     subject_id = subject_dir.split('/')[-1].split('-')[-1]
-    session_id = [x for x in subject_dir.split('/') if 'ses' in x][0].split('-')[-1]
+
     
     #read MRIQC pipeline output log
     output_log = subject_dir + '/mriqc_out_' + str(subject_id) + '.log'
 
     f = open(output_log, 'r')
     
+    #check if participant successfully passed MRIQC pipeline
     if "Participant level finished successfully." in f.read(): 
+        
+        #create path string
         acq_bold = subject_dir + '/' + subject_id + '_ses-' + session_id + '*_task-rest_run-*_bold*'
     
+    	#check if string is existing path
         if glob.glob(acq_bold):
                 return 'SUCCESS'
         else: return 'FAIL'
@@ -55,6 +64,7 @@ def check_bold(subject_dir):
         return 'FAIL'
          
 
+#eval_mriqc returns participant_id and pass/fail while check_bold only returns pass/fail
 tracker_configs = {
     "pipeline_complete": eval_mriqc,
     
