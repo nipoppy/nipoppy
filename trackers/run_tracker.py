84793c22582436f4541f660dbace96a667424840
Michelle Wang <tomichellewang@gmail.com>
2023-04-17 11:38:52 -0400

    fix path to manifest

--- trackers/run_tracker.py
+++ trackers/run_tracker.py
@@ -36,7 +36,7 @@ def run(global_config_file, dash_schema_file, pipelines, run_id=1):
 
         n_sessions = len(session_ids)
 
-        mr_proc_manifest = f"{mr_proc_root_dir}/tabular/demographics/mr_proc_manifest.csv"
+        mr_proc_manifest = f"{mr_proc_root_dir}/tabular/mr_proc_manifest.csv"
         manifest_df = pd.read_csv(mr_proc_manifest)
         participants = manifest_df[~manifest_df["bids_id"].isna()]["bids_id"].astype(str).str.strip().values
         n_participants = len(participants)
