c117944d8a9d73ace8756c334da5113a3222ae94
Michelle Wang <tomichellewang@gmail.com>
2023-05-31 18:27:42 -0400

    add testing flag and don't output prefixed global columns for now

--- trackers/run_tracker.py
+++ trackers/run_tracker.py
@@ -1,8 +1,13 @@
-import pandas as pd
-from pathlib import Path
+#!/usr/bin/env python
+
 import argparse
-from tracker import tracker, get_start_time
-import fs_tracker, fmriprep_tracker, mriqc_tracker
+from pathlib import Path
+
+import pandas as pd
+
+from trackers.tracker import tracker, get_start_time
+from trackers import fs_tracker, fmriprep_tracker, mriqc_tracker
+# from workflow.utils import load_manifest, save_backup # TODO once other PR is merged
 
 # Status flags
 SUCCESS="SUCCESS"
@@ -19,20 +24,23 @@ pipeline_tracker_config_dict = {
 }
 BIDS_PIPES = ["mriqc","fmriprep"]
 
-def run(global_config_file, dash_schema_file, pipelines, run_id=1):
+# number of participants to check per session when testing
+N_TESTING = 10
+
+def run(global_config_file, dash_schema_file, pipelines, run_id=1, testing=False):
     """ driver code running pipeline specific trackers
     """
 
     proc_status_dfs = [] # list of dataframes
     for pipeline in pipelines:
         pipe_tracker = tracker(global_config_file, dash_schema_file, pipeline) 
-            
+        
         mr_proc_root_dir, session_ids, version = pipe_tracker.get_global_configs()
         schema = pipe_tracker.get_dash_schema()
         tracker_configs = pipeline_tracker_config_dict[pipeline]
 
         mr_proc_manifest = f"{mr_proc_root_dir}/tabular/mr_proc_manifest.csv"
-        manifest_df = pd.read_csv(mr_proc_manifest)
+        manifest_df = pd.read_csv(mr_proc_manifest, converters={'datatype': pd.eval}) # TODO change to workflow.utils.load_manifest()
         participants = manifest_df[~manifest_df["bids_id"].isna()]["bids_id"].drop_duplicates().astype(str).str.strip().values
         n_participants = len(participants)
 
@@ -43,15 +51,18 @@ def run(global_config_file, dash_schema_file, pipelines, run_id=1):
 
         status_check_dict = pipe_tracker.get_pipe_tasks(tracker_configs, PIPELINE_STATUS_COLUMNS)
 
-        dash_col_list = list(schema["GLOBAL_COLUMNS"].keys()) 
-        
+        # only use non-prefixed columns at this stage
+        # for prefixed columns we need to generate the column name
+        dash_col_list = list(key for key, value in schema["GLOBAL_COLUMNS"].items() if not value["IsPrefixedColumn"])
+
         for session_id in session_ids:
             print(f"Checking session: {session_id}")    
             _df = pd.DataFrame(index=participants, columns=dash_col_list)          
             _df["session"] = session_id
-            _df["pipeline_name"] = pipeline        
+            _df["pipeline_name"] = pipeline
             _df["pipeline_version"] = version
             
+            n_participants = 0
             for bids_id in participants:
                 participant_id = manifest_df[manifest_df["bids_id"]==bids_id]["participant_id"].values[0]
                 _df.loc[bids_id,"participant_id"] = participant_id
@@ -74,12 +85,16 @@ def run(global_config_file, dash_schema_file, pipelines, run_id=1):
                         _df.loc[bids_id,name] = status
                         _df.loc[bids_id,"pipeline_starttime"] = get_start_time(subject_dir)
                         _df.loc[bids_id,"pipeline_endtime"] = UNAVAILABLE # TODO
+                    n_participants += 1
                 else:
                     # print(f"Pipeline output not found for bids_id: {bids_id}, session: {session}")
                     for name in status_check_dict.keys():                    
                         _df.loc[bids_id,name] = UNAVAILABLE
                         _df.loc[bids_id,"pipeline_starttime"] = UNAVAILABLE
                         _df.loc[bids_id,"pipeline_endtime"] = UNAVAILABLE
+
+                if testing and n_participants > N_TESTING:
+                    break
 
             proc_status_dfs.append(_df)
 
@@ -102,6 +117,7 @@ if __name__ == '__main__':
     parser.add_argument('--global_config', type=str, help='path to global config file for your mr_proc dataset', required=True)
     parser.add_argument('--dash_schema', type=str, help='path to dashboard schema to display tracker status', required=True)
     parser.add_argument('--pipelines', nargs='+', help='list of pipelines to track', required=True)
+    parser.add_argument('--testing', action='store_true', help=f'only check first {N_TESTING} participants with MRI data')
     args = parser.parse_args()
 
     # read global configs
@@ -113,4 +129,6 @@ if __name__ == '__main__':
 
     print(f"Tracking pipelines: {pipelines}")
 
-    run(global_config_file, dash_schema_file, pipelines)
+    testing = args.testing
+
+    run(global_config_file, dash_schema_file, pipelines, testing=testing)
