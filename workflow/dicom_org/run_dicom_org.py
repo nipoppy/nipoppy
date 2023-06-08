cd128edc5b083a46938e72948814e802b8c99d84
Michelle Wang <tomichellewang@gmail.com>
2023-06-08 16:00:01 -0400

    Refactor DICOM-to-BIDS conversion to use `doughnut.csv` (#89)

--- workflow/dicom_org/run_dicom_org.py
+++ workflow/dicom_org/run_dicom_org.py
@@ -1,12 +1,21 @@
-import pandas as pd
-import numpy as np
-from pathlib import Path
+#!/usr/bin/env python
+
 import argparse
 import json
 import workflow.logger as my_logger
-from workflow.dicom_org.utils import search_dicoms, copy_dicoms
 from joblib import Parallel, delayed
+from pathlib import Path
+
 import workflow.catalog as catalog
+from workflow.dicom_org.utils import search_dicoms, copy_dicoms
+from workflow.utils import (
+    COL_ORG_STATUS, 
+    DNAME_BACKUPS_STATUS, 
+    load_status,
+    participant_id_to_dicom_id, 
+    save_backup,
+    session_id_to_bids_session,
+)
 
 #Author: nikhil153
 #Date: 07-Oct-2022
@@ -23,7 +32,7 @@ def reorg(participant, participant_dicom_dir, raw_dicom_dir, dicom_dir, invalid_
     logger.info(f"n_raw_dicom: {len(raw_dcm_list)}, n_skipped (invalid/derived): {len(invalid_dicom_list)}")
 
     # Remove non-alphanumeric chars (e.g. "_" from the participant_dir names)
-    dicom_id = ''.join(filter(str.isalnum, participant))
+    dicom_id = participant_id_to_dicom_id(participant)
     participant_dicom_dir = f"{dicom_dir}/{dicom_id}/"
     
     copy_dicoms(raw_dcm_list, participant_dicom_dir, use_symlinks)
@@ -39,16 +48,17 @@ def reorg(participant, participant_dicom_dir, raw_dicom_dir, dicom_dir, invalid_
 def run(global_configs, session_id, logger=None, use_symlinks=True, skip_dcm_check=False, n_jobs=4):
     """ Runs the dicom reorg tasks 
     """
-    session = f"ses-{session_id}"
+    session = session_id_to_bids_session(session_id)
 
     # populate relative paths
     DATASET_ROOT = global_configs["DATASET_ROOT"]
     raw_dicom_dir = f"{DATASET_ROOT}/scratch/raw_dicom/{session}/"
     dicom_dir = f"{DATASET_ROOT}/dicom/{session}/"
-    log_dir = f"{DATASET_ROOT}/scratch/logs/"    
+    log_dir = f"{DATASET_ROOT}/scratch/logs/"
     invalid_dicom_dir = f"{log_dir}/invalid_dicom_dir/"
 
-    mr_proc_manifest = f"{DATASET_ROOT}/tabular/mr_proc_manifest.csv"
+    fpath_status = f"{DATASET_ROOT}/scratch/raw_dicom/doughnut.csv"
+    df_status = load_status(fpath_status)
     
     if logger is None:
         log_file = f"{log_dir}/dicom_org.log"
@@ -60,7 +70,7 @@ def run(global_configs, session_id, logger=None, use_symlinks=True, skip_dcm_che
     logger.info(f"session: {session}")
     logger.info(f"Number of parallel jobs: {n_jobs}")
 
-    reorg_df = catalog.get_new_raw_dicoms(mr_proc_manifest, raw_dicom_dir, dicom_dir, session_id, logger)
+    reorg_df = catalog.get_new_raw_dicoms(fpath_status, session_id, logger)
     n_dicom_reorg_participants = len(reorg_df)
 
     # start reorganizing
@@ -88,21 +98,24 @@ def run(global_configs, session_id, logger=None, use_symlinks=True, skip_dcm_che
         logger.info(f"Skipped (invalid/derived) DICOMs are listed here: {log_dir}")
         logger.info(f"DICOMs are now copied into {dicom_dir} and ready for bids conversion!")
 
+        reorg_df[COL_ORG_STATUS] = True
+        df_status.loc[reorg_df.index] = reorg_df
+        save_backup(df_status, fpath_status, DNAME_BACKUPS_STATUS)
+
     else:
         logger.info(f"No new participants found for dicom reorg...")
         
     logger.info("-"*50)
 
-
 if __name__ == '__main__':
     # argparse
     HELPTEXT = """
-    Script to reorganize raw (scanner dump) DICOMs into flatterned dir structure needed for BIDS conversion using HeuDiConv
+    Script to reorganize raw (scanner dump) DICOMs into flattened dir structure needed for BIDS conversion using HeuDiConv
     """
     parser = argparse.ArgumentParser(description=HELPTEXT)
-    parser.add_argument('--global_config', type=str, help='path to global config file for your mr_proc dataset')
-    parser.add_argument('--session_id', type=str, default=None, help='session (i.e. visit to process)')
-    parser.add_argument('--use_symlinks', action='store_true', help='symlink from raw_dicom to dicom to avoid duplication')
+    parser.add_argument('--global_config', type=str, help='path to global config file for your mr_proc dataset', required=True)
+    parser.add_argument('--session_id', type=str, help='session (i.e. visit to process)', required=True)
+    parser.add_argument('--no_symlinks', action='store_true', help='copy/duplicate files from raw_dicom to dicom (default: create symlinks)')
     parser.add_argument('--skip_dcm_check', action='store_true', help='skip raw dicoms checks to see if they are derived')
     parser.add_argument('--n_jobs', type=int, default=4, help='number of parallel processes')
     args = parser.parse_args()
@@ -113,7 +126,7 @@ if __name__ == '__main__':
         global_configs = json.load(f)
 
     session_id = args.session_id
-    use_symlinks = args.use_symlinks # Saves space and time! 
+    use_symlinks = not args.no_symlinks # Saves space and time! 
     skip_dcm_check = args.skip_dcm_check
     n_jobs = args.n_jobs
 
