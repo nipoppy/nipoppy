3bcc3ef71b48b3ef710964169aa4ac75e7212c37
nikhil153 <nikhil153@gmail.com>
2023-08-07 16:31:34 -0400

    updated nipoppy.py to call generate manifest

--- nipoppy.py
+++ nipoppy.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import argparse
 import json
 import workflow.logger as my_logger
+from workflow.tabular import generate_manifest
 from workflow.dicom_org import run_dicom_org
 from workflow.bids_conv import run_bids_conv
 
@@ -12,7 +13,7 @@ Top level script to orchestrate workflows as specified in the global_config.json
 """
 parser = argparse.ArgumentParser(description=HELPTEXT)
 parser.add_argument('--global_config', type=str, required=True, help='path to global config file for your nipoppy dataset')
-parser.add_argument('--session_id', type=str, required=True, help='current session or visit ID for the dataset')
+parser.add_argument('--session', type=str, required=True, help='current session or visit ID for the dataset')
 parser.add_argument('--n_jobs', type=int, default=4, help='number of parallel processes')
 
 args = parser.parse_args()
@@ -26,14 +27,16 @@ DATASET_ROOT = global_configs["DATASET_ROOT"]
 log_dir = f"{DATASET_ROOT}/scratch/logs/"
 log_file = f"{log_dir}/nipoppy.log"
 
-session_id = args.session_id
+session = args.session
 n_jobs = args.n_jobs
 
 logger = my_logger.get_logger(log_file)
 
 logger.info("-"*75)
 logger.info(f"Starting nipoppy for {DATASET_ROOT} dataset...")
-logger.info(f"dataset session (i.e visit): {session_id}")
+logger.info("-"*75)
+
+logger.info(f"dataset session (i.e visit): {session}")
 logger.info(f"Running {n_jobs} jobs in parallel")
 
 workflows = global_configs["WORKFLOWS"]
@@ -42,14 +45,22 @@ logger.info(f"Running {workflows} serially")
 for wf in workflows:
     logger.info("-"*50)
     logger.info(f"Starting workflow: {wf}")
-    if wf == "dicom_org":        
-        run_dicom_org.run(global_configs, session_id, n_jobs=n_jobs)
+    logger.info("-"*50)
+
+    if wf == "generate_manifest":
+        logger.info(f"***All sessions are fetched while generating manifest***")
+        generate_manifest.run(global_configs, task="regenerate", dash_bagel=True, logger=logger)
+    elif wf == "dicom_org":        
+        run_dicom_org.run(global_configs, session, n_jobs=n_jobs)
     elif wf == "bids_conv": 
-        run_bids_conv.run(global_configs, session_id, n_jobs=n_jobs)
+        run_bids_conv.run(global_configs, session, n_jobs=n_jobs)
     else:
         logger.error(f"Unknown workflow: {wf}")
+
+    logger.info("-"*50)
     logger.info(f"Finishing workflow: {wf}")
     logger.info("-"*50)
 
+logger.info("-"*75)
 logger.info(f"Finishing nipoppy run...")
 logger.info("-"*75)
--- workflow/tabular/generate_manifest.py
+++ workflow/tabular/generate_manifest.py
@@ -22,7 +22,7 @@ DASH_INDEX_COLUMNS = ["participant_id", "session", "bids_id"]
 DASH_NAME_COL = "assessment_name"
 DASH_VAL_COL = "assessment_score"
 
-def api_call(url, query):
+def api_call(url, query, logger):
     r = requests.post(url, data=query, verify=False)
     http_status = str(r.status_code)
     logger.info(f'HTTP Status: {http_status}')
@@ -36,100 +36,110 @@ def api_call(url, query):
 
     return query_df
 
-# argparse
-HELPTEXT = """
-Top level script to orchestrate workflows as specified in the global_config.json
-"""
-parser = argparse.ArgumentParser(description=HELPTEXT)
-parser.add_argument('--global_config', type=str, required=True, help='path to global config file for your dataset')
-parser.add_argument('--task', type=str, required=True, help='specify either regenerate or update the manifest')
-parser.add_argument('--query_label', type=str, default="Q1", help='query label to run')
-parser.add_argument('--dash_bagel', action='store_true', help='generate bagel for dashboard')
+def run(global_configs, task, query_label="Q1", dash_bagel=True, logger=None):
+    # load config
+    DATASET_ROOT = global_configs["DATASET_ROOT"]
+    log_dir = f"{DATASET_ROOT}/scratch/logs/"
+    redcap_config_json = f"{DATASET_ROOT}/proc/.redcap.json"
+    redcap_config = json.load(open(redcap_config_json))
 
-# args
-args = parser.parse_args()
+    # logger
+    if logger is None:
+        log_file = f"{log_dir}/bids_conv.log"
+        logger = my_logger.get_logger(log_file)
 
-global_configs = json.load(open(args.global_config))
-task = args.task
-query_label = args.query_label
-dash_bagel = args.dash_bagel
+    # redcap config
+    url = redcap_config["url"]
+    query = redcap_config["queries"][query_label]
 
-# load config
-DATASET_ROOT = global_configs["DATASET_ROOT"]
-log_dir = f"{DATASET_ROOT}/scratch/logs/"
-redcap_config_json = f"{DATASET_ROOT}/proc/.redcap.json"
-redcap_config = json.load(open(redcap_config_json))
+    # run query
+    logger.info(f"Running query {query_label}...")
+    query_df = api_call(url, query, logger=logger)
 
-# logger
-log_file = f"{log_dir}/bids_conv.log"
-logger = my_logger.get_logger(log_file)
+    # query_df = pd.read_csv("tmp_redcap_df.csv")
+    # # save query df local to avoid frequent API calls
+    # # query_df.to_csv("tmp_redcap_df.csv", index=False)
 
-# redcap config
-url = redcap_config["url"]
-query = redcap_config["queries"][query_label]
+    logger.info("Query results:")
+    logger.info(query_df.head())
 
-# run query
-logger.info(f"Running query {query_label}...")
-query_df = api_call(url, query)
+    # get the list of participants
+    redcap_participants = query_df["record_id"].unique()
+    n_participants = len(redcap_participants)
 
-# query_df = pd.read_csv("tmp_redcap_df.csv")
-# # save query df local to avoid frequent API calls
-# # query_df.to_csv("tmp_redcap_df.csv", index=False)
+    # get the list of sessions
+    redcap_sessions = query_df["redcap_event_name"].unique()
+    n_sessions = len(redcap_sessions)
 
-logger.info("Query results:")
-logger.info(query_df.head())
+    logger.info(f"Fetched {n_participants} participants and {n_sessions} sessions: {redcap_sessions}")
 
-# get the list of participants
-redcap_participants = query_df["record_id"].unique()
-n_participants = len(redcap_participants)
+    # generate manifest
+    if task == "regenerate":
+        logger.info("Generating manifest...")
 
-# get the list of sessions
-redcap_sessions = query_df["redcap_event_name"].unique()
-n_sessions = len(redcap_sessions)
+        manifest_df = pd.DataFrame(columns=MANIFEST_COLUMNS)
 
-logger.info(f"Fetched {n_participants} participants and {n_sessions} sessions: {redcap_sessions}")
+        manifest_df["participant_id"] = query_df["record_id"].copy()
+        manifest_df["visit"] = query_df["redcap_event_name"].copy()
+        manifest_df["session"] = query_df["redcap_event_name"].copy()
+        manifest_df["session"] = manifest_df["session"].replace(SESSION_MAP)
+        manifest_df["datatype"] = f"{DATATYPES}"
 
-# generate manifest
-logger.info("Generating manifest...")
+        manifest_df["bids_id"] = manifest_df["participant_id"].apply(utils.participant_id_to_bids_id)
 
-manifest_df = pd.DataFrame(columns=MANIFEST_COLUMNS)
+        # save manifest
+        now = datetime.now() # current date and time
+        timestamp = now.strftime("%Y%m%d")
 
-manifest_df["participant_id"] = query_df["record_id"].copy()
-manifest_df["visit"] = query_df["redcap_event_name"].copy()
-manifest_df["session"] = query_df["redcap_event_name"].copy()
-manifest_df["session"] = manifest_df["session"].replace(SESSION_MAP)
-manifest_df["datatype"] = f"{DATATYPES}"
+        manifest_fpath = f"{DATASET_ROOT}/tabular/manifest.csv"
+        manifest_bkup_dpath = f"{DATASET_ROOT}/tabular/.manifests"
+        manifest_bkup_fpath = f"{manifest_bkup_dpath}/manifest_{timestamp}.csv"
 
-manifest_df["bids_id"] = manifest_df["participant_id"].apply(utils.participant_id_to_bids_id)
+        Path(manifest_bkup_dpath).mkdir(parents=True, exist_ok=True)
 
-# save manifest
-now = datetime.now() # current date and time
-timestamp = now.strftime("%Y%m%d")
+        manifest_df.to_csv(manifest_fpath, index=False)
+        manifest_df.to_csv(manifest_bkup_fpath, index=False)
 
-manifest_fpath = f"{DATASET_ROOT}/tabular/manifest.csv"
-manifest_bkup_dpath = f"{DATASET_ROOT}/tabular/.manifests"
-manifest_bkup_fpath = f"{manifest_bkup_dpath}/manifest_{timestamp}.csv"
+        logger.info(f"Current manifest saved to {manifest_fpath} and backuped to: {manifest_bkup_fpath}")
+        logger.info(manifest_df.head())
 
-Path(manifest_bkup_dpath).mkdir(parents=True, exist_ok=True)
+    else:
+        logger.info("Updating manifest...")
+        # TODO: implement update manifest
 
-manifest_df.to_csv(manifest_fpath, index=False)
-manifest_df.to_csv(manifest_bkup_fpath, index=False)
+    # generate bagel for dashboard
+    if dash_bagel:
+        logger.info("Generating bagel for dashboard...")
 
-logger.info(f"Current manifest saved to {manifest_fpath} and backuped to: {manifest_bkup_fpath}")
-logger.info(manifest_df.head())
+        dash_df = query_df.copy()
+        dash_df = dash_df.rename(columns={"record_id": "participant_id", "redcap_event_name": "session"})
+        dash_df["bids_id"] = dash_df["participant_id"].apply(utils.participant_id_to_bids_id)
+        dash_df["session"] = dash_df["session"].replace(SESSION_MAP)
 
-# generate bagel for dashboard
-if dash_bagel:
-    logger.info("Generating bagel for dashboard...")
-    
-    dash_df = query_df.copy()
-    dash_df = dash_df.rename(columns={"record_id": "participant_id", "redcap_event_name": "session"})
-    dash_df["bids_id"] = dash_df["participant_id"].apply(utils.participant_id_to_bids_id)
-    dash_df["session"] = dash_df["session"].replace(SESSION_MAP)
+        dash_df_melt = dash_df.melt(id_vars=DASH_INDEX_COLUMNS, var_name=DASH_NAME_COL, value_name=DASH_VAL_COL)
 
-    dash_df_melt = dash_df.melt(id_vars=DASH_INDEX_COLUMNS, var_name=DASH_NAME_COL, value_name=DASH_VAL_COL)
+        # save bagel
+        dash_fpath = f"{DATASET_ROOT}/tabular/bagel.csv"
+        dash_df_melt.to_csv(dash_fpath, index=False)
+        logger.info(f"Bagel saved to {dash_fpath}")
 
-    # save bagel
-    dash_fpath = f"{DATASET_ROOT}/tabular/bagel.csv"
-    dash_df_melt.to_csv(dash_fpath, index=False)
-    logger.info(f"Bagel saved to {dash_fpath}")
+if __name__ == '__main__':
+    # argparse
+    HELPTEXT = """
+    Script to generate manifest and tabular data bagels for the dashboard
+    """
+    parser = argparse.ArgumentParser(description=HELPTEXT)
+    parser.add_argument('--global_config', type=str, required=True, help='path to global config file for your dataset')
+    parser.add_argument('--task', type=str, default="regenerate", required=True, help='specify either regenerate or update the manifest')
+    parser.add_argument('--query_label', type=str, default="Q1", help='query label to run')
+    parser.add_argument('--dash_bagel', action='store_true', help='generate bagel for dashboard')
+
+    # args
+    args = parser.parse_args()
+
+    global_configs = json.load(open(args.global_config))
+    task = args.task
+    query_label = args.query_label
+    dash_bagel = args.dash_bagel
+
+    run(global_configs, task, query_label, dash_bagel)
