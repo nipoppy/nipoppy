094a8277f5ec11af893c099d040192af84da720d
Remi Gau <remi_gau@hotmail.com>
2023-08-16 08:15:33 -0400

    refactor test

--- tests/test_workflow_fmriprep.py
+++ tests/test_workflow_fmriprep.py
@@ -1,35 +1,44 @@
+import pytest
+
 from nipoppy.workflow.proc_pipe.fmriprep.run_fmriprep import run_fmriprep
 
 import nipoppy.workflow.logger as my_logger
 
-def test_run_fmriprep():
+@pytest.mark.parametrize("use_bids_filter", [True, False])
+@pytest.mark.parametrize("anat_only", [True, False])
+def test_run_fmriprep(use_bids_filter, anat_only):
     
-    use_bids_filter = False
-    anat_only = False
     log_file = "fmriprep.log"
 
-    CMD = run_fmriprep(participant_id='01', 
-                 bids_dir="bids_dir",
-                 fmriprep_dir="fmriprep_dir",
-                 fs_dir="fs_dir",
-                 templateflow_dir="templateflow_dir",
-                 SINGULARITY_CONTAINER="fmriprep.simg",
+    bids_dir="bids_dir"
+    fmriprep_dir = "fmriprep_dir"
+    fs_dir="fs_dir"
+    templateflow_dir="templateflow_dir"
+    participant_id = '01'
+    singularity_container="fmriprep.simg"
+
+    cmd = run_fmriprep(participant_id=participant_id, 
+                 bids_dir=bids_dir,
+                 fmriprep_dir=fmriprep_dir,
+                 fs_dir=fs_dir,
+                 templateflow_dir=templateflow_dir,
+                 SINGULARITY_CONTAINER=singularity_container,
                  use_bids_filter=use_bids_filter,
                  anat_only=anat_only,
                  logger=my_logger.get_logger(log_file=log_file))
     
-    assert CMD == [
+    expected_cmd = [
         'singularity', 'run', 
-            '-B', 'bids_dir:/data_dir', 
-            '-B', 'fmriprep_dir/output//fmriprep_home_01/:/home/fmriprep', 
+            '-B', f'{bids_dir}:/data_dir', 
+            '-B', f'{fmriprep_dir}/output//fmriprep_home_{participant_id}/:/home/fmriprep', 
             '--home', '/home/fmriprep', 
             '--cleanenv', 
-            '-B', 'fmriprep_dir/output/:/output', 
-            '-B', 'templateflow_dir:/templateflow', 
-            '-B', 'fmriprep_dir:/work', 
-            '-B', 'fs_dir:/fsdir/', 
-                'fmriprep.simg', '/data_dir', '/output', 'participant', 
-                    '--participant-label', '01', 
+            '-B', f'{fmriprep_dir}/output/:/output', 
+            '-B', f'{templateflow_dir}:/templateflow', 
+            '-B', f'{fmriprep_dir}:/work', 
+            '-B', f'{fs_dir}:/fsdir/', 
+                singularity_container, '/data_dir', '/output', 'participant', 
+                    '--participant-label', participant_id, 
                     '-w', '/work', 
                     '--output-spaces', 'MNI152NLin2009cAsym:res-2', 'anat', 'fsnative', 
                     '--fs-subjects-dir', '/fsdir/', 
@@ -43,3 +52,11 @@ def test_run_fmriprep():
                     '--omp-nthreads', '4', 
                     '--nthreads', '8', 
                     '--mem_mb', '4000']
+
+    if use_bids_filter:
+        expected_cmd += ['--bids-filter-file', '/data_dir/bids_filter.json']
+
+    if anat_only:
+        expected_cmd += ['--anat-only']
+
+    assert cmd == expected_cmd
