ec36c298c5780d805c574bd7eb9f8a670b7ed0a6
nikhil153 <nikhil153@gmail.com>
2023-08-21 10:39:01 -0400

    refactored and cleaned up mriqc run script

--- nipoppy/workflow/proc_pipe/mriqc/run_mriqc.py
+++ nipoppy/workflow/proc_pipe/mriqc/run_mriqc.py
@@ -1,30 +1,110 @@
 import subprocess
 import argparse
-# import json
+import json
+import nipoppy.workflow.logger as my_logger
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description='')
+def run(participant_id, global_configs, session_id, output_dir, modalities, logger=None):
+    """ Runs mriqc command
+    """
 
-parser.add_argument('--global_config', type=str, help='path to global configs for a given nipoppy dataset')
-parser.add_argument('--output_dir', type=str, help='overwrite path to put results in case of issues with default')
-parser.add_argument('--participant_id', type=str, help='subject ID to be processed')
-parser.add_argument('--session_id', type=str, help='session ID to be processed')
+    DATASET_ROOT = global_configs["DATASET_ROOT"]
+    CONTAINER_STORE = global_configs["CONTAINER_STORE"]
+    MRIQC_CONTAINER = global_configs["PROC_PIPELINES"]["mriqc"]["CONTAINER"]
+    MRIQC_VERSION = global_configs["PROC_PIPELINES"]["mriqc"]["VERSION"]
+    MRIQC_CONTAINER = MRIQC_CONTAINER.format(MRIQC_VERSION)
+    SINGULARITY_CONTAINER = f"{CONTAINER_STORE}{MRIQC_CONTAINER}"
 
-args = parser.parse_args()
+    bids_dir = f"{DATASET_ROOT}/bids/"
+    if output_dir is None:
+        output_dir = f"{DATASET_ROOT}/derivatives"
 
-DATASET_ROOT = args.global_config["DATASET_ROOT"]
-CONTAINER_STORE = args.global_config["CONTAINER_STORE"]
-#is currently mriqc_patch.simg
-MRIQC_CONTAINER = args.global_config["PROC_PIPELINES"]["mriqc"]["CONTAINER"]
+    # create output dir
+    mriqc_output_dir = f"{output_dir}/mriqc/v{MRIQC_VERSION}/output/"
+    Path(mriqc_output_dir).mkdir(parents=True, exist_ok=True)
 
-# config = json.load(open(args.global_config))
+    # create working dir (intermediate files)
+    mriqc_work_dir = f"{output_dir}/mriqc/v{MRIQC_VERSION}/work/"
+    Path(mriqc_work_dir).mkdir(parents=True, exist_ok=True)
 
-output_dir = args.output_dir
-participant_id = args.participant_id
-session_id = args.session_id
+    # logging
+    log_dir = f"{DATASET_ROOT}/scratch/logs/"
 
-subprocess.run([f"echo subject: {participant_id} session: {session_id} >> {output_dir}/mriqc_out_{participant_id}.log"], shell=True)
+    if logger is None:
+        log_file = f"{log_dir}/mriqc.log"
+        logger = my_logger.get_logger(log_file)
 
-subprocess.run([f"singularity run --cleanenv -B {DATASET_ROOT}:/data:ro -B {output_dir}:/out --no-sub /data /out \
-                {CONTAINER_STORE}/{MRIQC_CONTAINER} participant --participant-label {participant_id} --session-id {session_id} \
-                >> {output_dir}/mriqc_out_{participant_id}.log"], shell=True)
+    logger.info("Starting mriqc run...")
+    logger.info(f"participant: {participant_id}, session: {session_id}")
+    logger.info(f"bids_dir: {bids_dir}")
+    logger.info(f"output_dir: {output_dir}")
+    logger.info(f"work_dir: {mriqc_work_dir}")
 
+    # Singularity CMD 
+    SINGULARITY_CMD=f"singularity run \
+        -B {bids_dir}:/data:ro \
+        -B {mriqc_output_dir}:/out \
+        -B {mriqc_work_dir}:/work \
+        {SINGULARITY_CONTAINER} "
+
+    # Compose mriqc command
+    modalities_str = " ".join(modalities)
+    MRIQC_CMD=f"/data /out participant \
+        --participant-label {participant_id} \
+        --session-id {session_id} \
+        --modalities {modalities_str} \
+        --no-sub \
+        --work-dir /work \
+        --bids-database-wipe"
+        # --bids-database-dir /out/bids_db/{participant_id}"
+
+    CMD_ARGS = SINGULARITY_CMD + MRIQC_CMD 
+    CMD = CMD_ARGS.split()
+
+    logger.info(f"Running mriqc container...")
+    logger.info("-"*50)
+    logger.info(f"CMD:\n{CMD}")
+    logger.info("-"*50)
+    
+    try:
+        mriqc_proc = subprocess.run(CMD)
+        if mriqc_proc.returncode == 0:
+            logger.info(f"Successfully completed mriqc run for participant: {participant_id}")
+        else:
+            logger.error(f"mriqc run failed with return code: {mriqc_proc.returncode}, for participant: {participant_id}")
+
+    except Exception as e:
+        logger.error(f"mriqc run failed with exceptions: {e}")
+
+
+if __name__ == '__main__':
+
+    # argparse
+    HELPTEXT = """
+    Script to run mriqc 
+    """
+    parser = argparse.ArgumentParser(description=HELPTEXT)
+
+    parser.add_argument('--global_config', type=str, required=True, help='path to global configs for a given mr_proc dataset')
+    parser.add_argument('--participant_id', type=str, required=True, help='participant id')
+    parser.add_argument('--session_id', type=str, required=True, help='session id for the participant')
+    parser.add_argument('--output_dir', type=str, default=None, help='specify custom output dir (if None, output is saved at <DATASET_ROOT>/derivatives/mriqc)')
+    parser.add_argument('--modalities', nargs="*", required=True, help='specify modalities (i.e. suffixes) to QC. Possible options: T1w, T2w, bold, dwi')
+
+    args = parser.parse_args()
+
+    global_config_file = args.global_config
+    participant_id = args.participant_id
+    session_id = args.session_id
+    output_dir = args.output_dir
+    modalities = args.modalities
+
+    # Read global configs
+    with open(global_config_file, 'r') as f:
+        global_configs = json.load(f)
+
+    output_dir = args.output_dir
+    participant_id = args.participant_id
+    session_id = args.session_id
+
+    run(participant_id, global_configs, session_id, output_dir, modalities)
