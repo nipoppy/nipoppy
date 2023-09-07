56d1b5e8c0aa087a6a0fbcc4a7fcf054595cc400
Michelle Wang <tomichellewang@gmail.com>
2023-09-07 15:21:54 -0400

    update by session+pipeline instead of pipeline only

--- nipoppy/trackers/run_tracker.py
+++ nipoppy/trackers/run_tracker.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import argparse
 import json
 from pathlib import Path
@@ -49,8 +48,7 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
     else:
         session_ids = [session_id]
 
-    logger.info(f"tracking session_ids: {session_ids}")
-    
+    logger.info(f"tracking session_ids: {session_ids}")    
 
     for pipeline in pipelines:
         pipe_tracker = Tracker(global_configs, dash_schema_file, pipeline) 
@@ -65,26 +63,6 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
         participants_total = doughnut_df[doughnut_df[COL_CONV_STATUS]][COL_BIDS_ID_MANIFEST].unique()
         n_participants_total = len(participants_total)
 
-        # TODO change to check/update session/pipeline chunk instead of pipeline chunk
-        tracker_csv = Path(mr_proc_root_dir, 'derivatives', FNAME_BAGEL)
-        if tracker_csv.exists():
-            old_proc_status_df_full = load_bagel(tracker_csv)
-
-            old_participants = set(old_proc_status_df_full['bids_id'])
-            old_pipelines = set(old_proc_status_df_full['pipeline_name'])
-            
-            # make sure the number of participants is consistent across pipelines
-            if set(participants_total) != old_participants and not old_pipelines.issubset(set(pipelines)):
-                raise RuntimeError(
-                    'The existing processing status file might be obsolete (participant list does not match the status file)'
-                    f'. Rerun the tracker script with --pipelines {" ".join(old_pipelines.union(pipelines))}'
-                )
-            
-            old_proc_status_df = old_proc_status_df_full.loc[~((old_proc_status_df_full["pipeline_name"] == pipeline) & (old_proc_status_df_full["pipeline_version"] == version))]
-            
-        else:
-            old_proc_status_df = None
-
         logger.info("-"*50)
         logger.info(f"pipeline: {pipeline}, version: {version}")
         logger.info(f"n_participants_total: {n_participants_total}, session_ids: {session_ids}")
@@ -96,7 +74,6 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
         # for prefixed columns we need to generate the column name
         dash_col_list = list(key for key, value in schema["GLOBAL_COLUMNS"].items() if not value["IsPrefixedColumn"])
 
-        proc_status_session_dfs = [] # list of dataframes
         for session_id in session_ids:
             session = session_id_to_bids_session(session_id)
             logger.info(f"Checking session: {session}")
@@ -109,7 +86,33 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
             _df[COL_SESSION_MANIFEST] = session
             _df["pipeline_name"] = pipeline
             _df["pipeline_version"] = version
-            _df["has_mri_data"] = TRUE # everyone with a session value has MRI data
+            _df["has_mri_data"] = TRUE # everyone in the status file has MRI data
+
+            tracker_csv = Path(mr_proc_root_dir, 'derivatives', FNAME_BAGEL)
+            if tracker_csv.exists():
+                old_proc_status_df_full = load_bagel(tracker_csv)
+
+                old_proc_status_df_session = old_proc_status_df_full.loc[old_proc_status_df_full[COL_SESSION_MANIFEST] == session]
+                old_participants_session = set(old_proc_status_df_session[COL_BIDS_ID_MANIFEST])
+                old_pipelines_session = set(old_proc_status_df_session['pipeline_name'])
+                
+                # make sure the number of participants is consistent across pipelines
+                if set(participants_session) != old_participants_session and not old_pipelines_session.issubset(set(pipelines)):
+                    raise RuntimeError(
+                        f'The existing processing status file might be obsolete (participant list does not match the status file for session {session})'
+                        f'. Rerun the tracker script with --pipelines {" ".join(old_pipelines_session.union(pipelines))}'
+                    )
+                
+                old_proc_status_df = old_proc_status_df_full.loc[
+                    ~(
+                        (old_proc_status_df_full["pipeline_name"] == pipeline) &
+                        (old_proc_status_df_full["pipeline_version"] == version) &
+                        (old_proc_status_df_full[COL_SESSION_MANIFEST] == session)
+                    )
+                ]
+                
+            else:
+                old_proc_status_df = None
             
             for bids_id in participants_session:
                 participant_id = doughnut_df[doughnut_df[COL_BIDS_ID_MANIFEST]==bids_id][COL_SUBJECT_MANIFEST].values[0]
@@ -140,25 +143,22 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
                         _df.loc[bids_id,"pipeline_starttime"] = UNAVAILABLE
                         _df.loc[bids_id,"pipeline_endtime"] = UNAVAILABLE
 
-            proc_status_session_dfs.append(_df)
+            _df = _df.reset_index(drop=True)
 
-        # new rows for this pipeline
-        pipeline_proc_status_df = pd.concat(proc_status_session_dfs, axis='index').reset_index(drop=True)
+            # add old rows from other pipelines/sessions and sort for consistent order
+            proc_status_df: pd.DataFrame = pd.concat([old_proc_status_df, _df], axis='index')
+            proc_status_df = proc_status_df.sort_values(["pipeline_name", "pipeline_version", COL_BIDS_ID_MANIFEST], ignore_index=True)
 
-        # add old rows from other pipelines and sort for consistent order
-        proc_status_df: pd.DataFrame = pd.concat([old_proc_status_df, pipeline_proc_status_df], axis='index')
-        proc_status_df = proc_status_df.sort_values(["pipeline_name", "pipeline_version", COL_BIDS_ID_MANIFEST], ignore_index=True)
-
-        # don't write a new file if no changes
-        try:
-            if len(proc_status_df.compare(old_proc_status_df_full)) == 0:
-                logger.info(f'\nNo change for pipeline {pipeline}')
-                continue
-        except Exception:
-            pass
-        
-        # save proc_status_df
-        save_backup(proc_status_df, tracker_csv, DNAME_BACKUPS_BAGELS)
+            # don't write a new file if no changes
+            try:
+                if len(proc_status_df.compare(old_proc_status_df_full)) == 0:
+                    logger.info(f'\nNo change for pipeline {pipeline}, session {session}')
+                    continue
+            except Exception:
+                pass
+            
+            # save proc_status_df
+            save_backup(proc_status_df, tracker_csv, DNAME_BACKUPS_BAGELS)
 
 def load_bagel(fpath_bagel):
 
