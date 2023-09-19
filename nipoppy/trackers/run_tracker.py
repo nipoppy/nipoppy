0bfe0a03b291d15cc6afa0cacfba0b4052e37d27
nikhil153 <nikhil153@gmail.com>
2023-09-19 16:21:50 -0400

    updated bids_tracker to match proc_pipe tracker schema

--- nipoppy/trackers/run_tracker.py
+++ nipoppy/trackers/run_tracker.py
@@ -2,11 +2,14 @@ import pandas as pd
 from pathlib import Path
 import argparse
 from nipoppy.trackers.tracker import UNAVAILABLE, Tracker, get_start_time
-from nipoppy.trackers import fs_tracker, fmriprep_tracker, mriqc_tracker, tractoflow_tracker
+from nipoppy.trackers import bids_tracker, fs_tracker, fmriprep_tracker, mriqc_tracker, tractoflow_tracker
 import nipoppy.workflow.logger as my_logger
 import json
+import bids 
 
 from nipoppy.workflow.utils import (
+    BIDS_SUBJECT_PREFIX,
+    BIDS_SESSION_PREFIX,
     COL_SUBJECT_MANIFEST,
     COL_BIDS_ID_MANIFEST,
     COL_SESSION_MANIFEST,
@@ -18,6 +21,7 @@ from nipoppy.workflow.utils import (
 # Globals
 PIPELINE_STATUS_COLUMNS = "PIPELINE_STATUS_COLUMNS"
 pipeline_tracker_config_dict = {
+    "heudiconv": bids_tracker.tracker_configs, 
     "freesurfer": fs_tracker.tracker_configs,
     "fmriprep": fmriprep_tracker.tracker_configs,
     "mriqc": mriqc_tracker.tracker_configs,
@@ -30,6 +34,13 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
     """
     DATASET_ROOT = global_configs["DATASET_ROOT"]
 
+    # for bids tracker
+    bids_dir = f"{DATASET_ROOT}/bids/"
+    
+    # Grab BIDS participants from the doughnut
+    doughnut_file = f"{DATASET_ROOT}/scratch/raw_dicom/doughnut.csv"
+    doughnut_df = load_status(doughnut_file)
+    
     # logging
     log_dir = f"{DATASET_ROOT}/scratch/logs/"
     if logger is None:
@@ -39,54 +50,65 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
     logger.info(f"Tracking pipelines: {pipelines}")
 
     if session_id == "ALL":
-        session_ids = global_configs["SESSIONS"]
+        sessions = global_configs["SESSIONS"]
     else:
-        session_ids = [session_id]
+        sessions = [f"ses-{session_id}"]
 
-    logger.info(f"tracking session_ids: {session_ids}")
+    logger.info(f"tracking session_ids: {sessions}")
     
     proc_status_dfs = [] # list of dataframes
-    for session_id in session_ids:
-
-        session = session_id_to_bids_session(session_id)
-
+    for session in sessions:
+        logger.info(f"Checking session: {session}")  
+        session_id = str(session.removeprefix(BIDS_SESSION_PREFIX))
+        
         for pipeline in pipelines:
+            logger.info(f"Checking pipeline: {pipeline}")  
             pipe_tracker = Tracker(global_configs, dash_schema_file, pipeline) 
                 
             # TODO revise tracker class
-            DATASET_ROOT, session_ids, version = pipe_tracker.get_global_configs()
+            # DATASET_ROOT, session_ids, version = pipe_tracker.get_global_configs()
+            if pipeline == "heudiconv":
+                version = global_configs["BIDS"][pipeline]["VERSION"]
+            else:
+                version = global_configs["PROC_PIPELINES"][pipeline]["VERSION"]
+
             schema = pipe_tracker.get_dash_schema()
             tracker_configs = pipeline_tracker_config_dict[pipeline]
 
-            # Grab BIDS participants from the doughnut
-            doughnut_file = f"{DATASET_ROOT}/scratch/raw_dicom/doughnut.csv"
-            doughnut_df = load_status(doughnut_file)
             bids_participants = doughnut_df[(doughnut_df[COL_SESSION_MANIFEST]==session) & 
                                             (doughnut_df[COL_CONV_STATUS])][COL_BIDS_ID_MANIFEST].unique()
             n_bids_participants = len(bids_participants)
 
             logger.info("-"*50)
             logger.info(f"pipeline: {pipeline}, version: {version}")
-            logger.info(f"n_participants: {n_bids_participants}, session_ids: {session_ids}")
+            logger.info(f"n_participants: {n_bids_participants}, session_ids: {sessions}")
             logger.info("-"*50)
 
             status_check_dict = pipe_tracker.get_pipe_tasks(tracker_configs, PIPELINE_STATUS_COLUMNS, pipeline, version)
 
             dash_col_list = list(schema["GLOBAL_COLUMNS"].keys()) 
-            
-            logger.info(f"Checking session: {session_id}")    
+              
             _df = pd.DataFrame(index=bids_participants, columns=dash_col_list)          
             _df["session"] = session
             _df["pipeline_name"] = pipeline        
             _df["pipeline_version"] = version
+
+            # BIDS (i.e. heudiconv tracker is slightly different than proc_pipes)
+            if pipeline == "heudiconv":
+                # Generate BIDSLayout only once per tracker run and not for each participant
+                bids_layout = bids.BIDSLayout(bids_dir, validate=False)
+                logger.debug(f"bids_dir: {bids_dir}")
+                logger.debug(f"bids_layout: {bids_layout.get_subjects()}")
             
             for bids_id in bids_participants:
                 participant_id = doughnut_df[doughnut_df[COL_BIDS_ID_MANIFEST]==bids_id][COL_SUBJECT_MANIFEST].values[0]
                 _df.loc[bids_id,COL_SUBJECT_MANIFEST] = participant_id
                 logger.debug(f"bids_id: {bids_id}, participant_id: {participant_id}")
 
-                if pipeline == "freesurfer":
-                    subject_dir = f"{DATASET_ROOT}/derivatives/{pipeline}/v{version}/output/ses-{session_id}/{bids_id}" 
+                if pipeline == "heudiconv":
+                    subject_dir = f"{DATASET_ROOT}/bids/{bids_id}"
+                elif pipeline == "freesurfer":
+                    subject_dir = f"{DATASET_ROOT}/derivatives/{pipeline}/v{version}/output/{session}/{bids_id}" 
                 elif pipeline in BIDS_PIPES:
                     subject_dir = f"{DATASET_ROOT}/derivatives/{pipeline}/v{version}/output/{bids_id}" 
                 else:
@@ -97,7 +119,11 @@ def run(global_configs, dash_schema_file, pipelines, session_id="ALL", run_id=1,
                 
                 if dir_status:                
                     for name, func in status_check_dict.items():
-                        status = func(subject_dir, session, run_id)
+                        if pipeline == "heudiconv":
+                            status = func(bids_layout, participant_id, session_id, run_id)
+                        else:
+                            status = func(subject_dir, session_id, run_id)
+
                         logger.debug(f"task_name: {name}, status: {status}")
                         _df.loc[bids_id,name] = status
                         _df.loc[bids_id,"pipeline_starttime"] = get_start_time(subject_dir)
