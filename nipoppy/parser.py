f6d4d321a7aa017cf8c2e2c78aadc5efe640c826
Michelle Wang <tomichellewang@gmail.com>
2023-11-03 14:24:33 -0400

    change --global-configs to keyword argument and add docstrings

--- nipoppy/parser.py
+++ nipoppy/parser.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import argparse
 from pathlib import Path
+from typing import Sequence
 
 from nipoppy.base import GlobalConfigs
 from nipoppy.logger import create_logger
@@ -10,18 +11,36 @@ REQUIRED_ARG = 'REQUIRED'
 OPTIONAL_ARG = 'OPTIONAL'
 
 DEFAULT_LOG_LEVEL = 'INFO'
-
 VALID_LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 
 class CustomParser(argparse.ArgumentParser):
 
-    def __init__(self, description, **kwargs) -> None:
+    def __init__(self, description: str | None = None, **kwargs) -> None:
+        """A custom parser class with handling of logging and dry run arguments.
+
+        Parameters
+        ----------
+        description : str | None, optional
+            Description of the program, by default None
+        """
         super().__init__(description=description, **kwargs)
         self.logger_name = None
         self.with_logging_args = False
     
-    def parse_args(self):
-        args = super().parse_args()
+    def parse_args(self, args: Sequence[str] | None) -> argparse.Namespace:
+        """Parse arguments and create a logger if needed.
+
+        Parameters
+        ----------
+        args : Sequence[str] | None
+            The arguments to parse
+
+        Returns
+        -------
+        argparse.Namespace
+            The parsed arguments
+        """
+        args = super().parse_args(args=args)
         if self.with_logging_args:
             args.logger = create_logger(
                 name=self.logger_name,
@@ -39,13 +58,27 @@ class CustomParser(argparse.ArgumentParser):
             args.logger = None
         return args
 
-    def add_generic_optional_args(self, logger_name: str) -> CustomParser:
+    def add_generic_optional_args(self, logger_name: str):
+        """Add generic logging and dry run arguments to the parser.
+
+        Parameters
+        ----------
+        logger_name : str
+            Name of the logger to create.
+        """
         self.add_logging_args(logger_name=logger_name)
         self.add_dry_run_arg()
-        return self
     
-    def add_logging_args(self, logger_name: str) -> CustomParser:
+    def add_logging_args(self, logger_name: str):
+        """Add logging arguments to the parser.
 
+        The logging arguments are: --logfile, --log-level, --aggregate-logs.
+
+        Parameters
+        ----------
+        logger_name : str
+            Name of the logger to create
+        """
         def _log_level(log_level: str):
             log_level = log_level.upper()
             if log_level not in VALID_LOG_LEVELS:
@@ -61,6 +94,7 @@ class CustomParser(argparse.ArgumentParser):
         self.add_argument(
             '--logfile',
             '--log-path',
+            '--log_path',
             type=Path,
             required=False,
             help=(
@@ -89,27 +123,46 @@ class CustomParser(argparse.ArgumentParser):
             required=False,
             help='use a single logger for all processes.',
         )
-
-        return self
     
-    def add_dry_run_arg(self) -> CustomParser:
+    def add_dry_run_arg(self):
+        """Add a --dry-run argument to the parser.
+        """
 
         self.add_argument(
-            '--dry_run',
             '--dry-run',
+            '--dry_run',
             action='store_true',
             required=False,
             help='print commands without actually running them.',
         )
-
-        return self
 
 def get_base_parser(
         description: str | None = None,
         subject: str | None = None,
         session: str | None = None,
     ) -> CustomParser:
+    """Get a parser with some basic arguments.
 
+    This returns a CustomParser with a --global-config argument and, optionally,
+    --subject and/or --session arguments. It will also have methods to add
+    logging and dry run arguments.
+
+    Parameters
+    ----------
+    description : str | None, optional
+        Description of the program, by default None
+    subject : str | None, optional
+        Whether to add a required or optional subject argument. Accepted values
+        are "REQUIRED", "OPTIONAL", or None, by default None
+    session : str | None, optional
+        Whether to add a required or optional session argument. Accepted values
+        are "REQUIRED", "OPTIONAL", or None, by default None
+
+    Returns
+    -------
+    CustomParser
+        The parser object
+    """
     def check_required(arg_type):
         if arg_type not in [REQUIRED_ARG, OPTIONAL_ARG]:
             raise ValueError(
@@ -127,7 +180,8 @@ def get_base_parser(
     parser = CustomParser(description=description)
 
     parser.add_argument(
-        'global_configs',
+        '--global-configs',
+        '--global_configs',
         type=GlobalConfigs,
         help='path to global configs for a given dataset.',
     )
