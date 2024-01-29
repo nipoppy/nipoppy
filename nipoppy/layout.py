901322175bfb203da78027121a1336ce580c7f22
Michelle Wang <tomichellewang@gmail.com>
2024-01-29 17:49:11 -0500

    add method to create empty dataset

--- nipoppy/layout.py
+++ nipoppy/layout.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class DatasetLayout:
     """File/directory structure in a dataset."""
 
-    def __init__(self, dataset_root: Path | str):
+    def __init__(self, dpath_root: Path | str):
         """Initialize the object.
 
         Parameters
@@ -13,22 +13,44 @@ class DatasetLayout:
         dataset_root : Path | str
             Path to the root directory of the dataset.
         """
-        self.dataset_root = Path(dataset_root)
+        self.dpath_root = Path(dpath_root)
 
-        self.dpath_bids = self.dataset_root / "bids"
-        self.dpath_derivatives = self.dataset_root / "derivatives"
-        self.dpath_dicom = self.dataset_root / "dicom"
-        self.dpath_downloads = self.dataset_root / "downloads"
+        self.dpath_bids = self.dpath_root / "bids"
+        self.dpath_derivatives = self.dpath_root / "derivatives"
+        self.dpath_dicom = self.dpath_root / "dicom"
+        self.dpath_downloads = self.dpath_root / "downloads"
 
-        self.dpath_proc = self.dataset_root / "proc"
-        self.fpath_config = self.dataset_root / "global_configs.json"
+        self.dpath_proc = self.dpath_root / "proc"
+        self.fpath_config = self.dpath_root / "global_configs.json"
 
-        self.dpath_scratch = self.dataset_root / "scratch"
+        self.dpath_scratch = self.dpath_root / "scratch"
         self.dpath_raw_dicom = self.dpath_scratch / "raw_dicom"
         self.fpath_doughnut = self.dpath_raw_dicom / "doughnut.csv"
         self.dpath_logs = self.dpath_scratch / "logs"
 
-        self.dpath_tabular = self.dataset_root / "tabular"
+        self.dpath_tabular = self.dpath_root / "tabular"
         self.dpath_assessments = self.dpath_tabular / "assessments"
         self.dpath_demographics = self.dpath_tabular / "demographics"
         self.fpath_manifest = self.dpath_tabular / "manifest.csv"
+
+    def create(self):
+        """Create an empty dataset."""
+        if self.dpath_root.exists():
+            raise FileExistsError(f"Dataset already exists: {self.dpath_root}")
+
+        dpaths = [
+            self.dpath_root,
+            self.dpath_bids,
+            self.dpath_derivatives,
+            self.dpath_dicom,
+            self.dpath_downloads,
+            self.dpath_proc,
+            self.dpath_scratch,
+            self.dpath_raw_dicom,
+            self.dpath_logs,
+            self.dpath_tabular,
+            self.dpath_assessments,
+            self.dpath_demographics,
+        ]
+        for dpath in dpaths:
+            dpath.mkdir(parents=True, exist_ok=True)
--- tests/test_layout.py
+++ tests/test_layout.py
@@ -5,27 +5,52 @@ import pytest
 
 from nipoppy.layout import DatasetLayout
 
-ATTR_TO_PATH_MAP = {
+ATTR_TO_DPATH_MAP = {
     "dpath_bids": "bids",
     "dpath_derivatives": "derivatives",
     "dpath_dicom": "dicom",
     "dpath_downloads": "downloads",
     "dpath_proc": "proc",
-    "fpath_config": "global_configs.json",
     "dpath_scratch": "scratch",
     "dpath_raw_dicom": "scratch/raw_dicom",
-    "fpath_doughnut": "scratch/raw_dicom/doughnut.csv",
     "dpath_logs": "scratch/logs",
     "dpath_tabular": "tabular",
     "dpath_assessments": "tabular/assessments",
     "dpath_demographics": "tabular/demographics",
+}
+
+ATTR_TO_FPATH_MAP = {
+    "fpath_config": "global_configs.json",
+    "fpath_doughnut": "scratch/raw_dicom/doughnut.csv",
     "fpath_manifest": "tabular/manifest.csv",
 }
+
+
+@pytest.fixture(params=["my_dataset", "dataset_dir"])
+def dpath_root(request: pytest.FixtureRequest, tmp_path: Path) -> Path:
+    """Dataset root."""
+    return tmp_path / request.param
 
 
 @pytest.mark.parametrize("dataset_root", ["my_dataset", "dataset_dir"])
 def test_layout_init(dataset_root):
     """Test layout initialization."""
-    layout = DatasetLayout(dataset_root=dataset_root)
-    for attr, path in ATTR_TO_PATH_MAP.items():
+    layout = DatasetLayout(dpath_root=dataset_root)
+    for attr, path in {**ATTR_TO_DPATH_MAP, **ATTR_TO_FPATH_MAP}.items():
         assert getattr(layout, attr) == Path(dataset_root) / path
+
+
+def test_layout_create(dpath_root: Path):
+    """Test layout creation."""
+    layout = DatasetLayout(dpath_root=dpath_root)
+    layout.create()
+    for path in ATTR_TO_DPATH_MAP.values():
+        assert Path(dpath_root / path).exists()
+
+
+def test_layout_create_error(dpath_root: Path):
+    """Check that an error is raised if the dataset already exists."""
+    dpath_root.mkdir()
+    layout = DatasetLayout(dpath_root=dpath_root)
+    with pytest.raises(FileExistsError, match="Dataset already exists"):
+        layout.create()
