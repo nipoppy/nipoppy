72e85dda90bc0a5bf211c074471dd37c66f50f6e
Michelle Wang <tomichellewang@gmail.com>
2024-01-31 22:50:51 -0500

    add CLI test for init

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,10 @@
 """Tests for the CLI."""
+from pathlib import Path
+
 import pytest
 
 from nipoppy.cli.run import cli
+from nipoppy.dataset_init import DatasetInitWorkflow
 
 
 def test_cli():
@@ -14,3 +17,8 @@ def test_cli_invalid():
     with pytest.raises(SystemExit) as exception:
         cli(["nipoppy", "--fake-arg"])
     assert exception.value.code != 0
+
+
+def test_cli_init(tmp_path: Path):
+    output = cli(["nipoppy", "init", "--dataset-root", str(tmp_path / "my_dataset")])
+    assert isinstance(output, DatasetInitWorkflow)
