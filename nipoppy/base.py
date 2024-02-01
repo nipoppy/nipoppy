6c22b1f6df717f24592a0275f0d588824062ea07
Michelle Wang <tomichellewang@gmail.com>
2024-01-31 22:30:34 -0500

    improve default __str__ method and add tests for _Base

--- nipoppy/base.py
+++ nipoppy/base.py
@@ -1,4 +1,5 @@
 """Base class."""
+import inspect
 from abc import ABC
 
 
@@ -15,7 +16,21 @@ class _Base(ABC):
         return f"{type(self).__name__}({sep.join([str(c) for c in components])})"
 
     def __str__(self) -> str:
-        return self._str_helper()
+        signature = inspect.signature(type(self))
+        names = [
+            name
+            for name, parameter in signature.parameters.items()
+            if parameter.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+        ]
+        try:
+            return self._str_helper(names=names)
+        except AttributeError:
+            raise RuntimeError(
+                f"The __init__ method of the {type(self)} class has positional and/or"
+                " keyword arguments that are not set as attributes of the object"
+                ". Failed to build string representation: need to override the"
+                " __str__ method"
+            )
 
     def __repr__(self) -> str:
         return self.__str__()
