1f0dac46a4251d99629712fc832d5b09bb323afd
Michelle Wang <tomichellewang@gmail.com>
2024-04-10 16:19:25 -0400

    set up docs with sphinx

---
+++ nipoppy_new/docs/source/conf.py
@@ -0,0 +1,27 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Nipoppy"
+copyright = "2024, NeuroDataScience-ORIGAMI Lab"
+author = "NeuroDataScience-ORIGAMI Lab"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "sphinxarg.ext", "sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
