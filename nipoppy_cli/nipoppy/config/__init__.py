9cb3cf0c569c38d0de17969543fad1800b4139da
Michelle Wang <tomichellewang@gmail.com>
2024-07-08 10:22:47 -0400

    refactor PipelineConfig and PipelineStepConfig to add UPDATE_DOUGHNUT field for BIDS pipelines

--- nipoppy_cli/nipoppy/config/__init__.py
+++ nipoppy_cli/nipoppy/config/__init__.py
@@ -3,6 +3,6 @@
 from .boutiques import BoutiquesConfig
 from .container import ContainerConfig
 from .main import Config
-from .pipeline import PipelineConfig
-from .pipeline_step import PipelineStepConfig
+from .pipeline import BidsPipelineConfig, ProcPipelineConfig
+from .pipeline_step import BidsPipelineStepConfig, ProcPipelineStepConfig
 from .tracker import TrackerConfig
