bf6ad624676e9c59eeda92d23b29782742a21f21
Michelle Wang <tomichellewang@gmail.com>
2024-07-11 03:27:05 -0400

    fix docs warnings

--- nipoppy_cli/nipoppy/config/__init__.py
+++ nipoppy_cli/nipoppy/config/__init__.py
@@ -3,6 +3,10 @@
 from .boutiques import BoutiquesConfig
 from .container import ContainerConfig
 from .main import Config
-from .pipeline import BidsPipelineConfig, ProcPipelineConfig
-from .pipeline_step import BidsPipelineStepConfig, ProcPipelineStepConfig
+from .pipeline import BasePipelineConfig, BidsPipelineConfig, ProcPipelineConfig
+from .pipeline_step import (
+    BasePipelineStepConfig,
+    BidsPipelineStepConfig,
+    ProcPipelineStepConfig,
+)
 from .tracker import TrackerConfig
