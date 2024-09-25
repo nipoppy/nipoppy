3ab97312e627a49ac563425da660de908e946712
jacobsanz97 <jacobsanz97@hotmail.com>
2024-09-24 21:43:52 -0400

    Pybids database creation toggle

--- nipoppy/config/pipeline_step.py
+++ nipoppy/config/pipeline_step.py
@@ -80,6 +80,12 @@ class ProcPipelineStepConfig(BasePipelineStepConfig):
             "when building the PyBIDS layout"
         ),
     )
+    GENERATE_PYBIDS_DATABASE: Optional[bool] = Field(
+        default=True,
+        description=(
+            "Whether or not to generate a PyBIDS database as part of the pipeline step"
+        ),
+    )
     model_config = ConfigDict(extra="forbid")
 
 
