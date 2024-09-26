7ef30ca12497bff85ad33e751bd16938cc822cc3
Jacob Sanz-Robinson <47538991+jacobsanz97@users.noreply.github.com>
2024-09-26 14:37:05 -0400

    Update nipoppy/config/pipeline_step.py
    Co-authored-by: Michelle Wang <tomichellewang@gmail.com>

--- nipoppy/config/pipeline_step.py
+++ nipoppy/config/pipeline_step.py
@@ -83,7 +83,7 @@ class ProcPipelineStepConfig(BasePipelineStepConfig):
     GENERATE_PYBIDS_DATABASE: Optional[bool] = Field(
         default=True,
         description=(
-            "Whether or not to generate a PyBIDS database as part of the pipeline step"
+            "Whether or not to generate a PyBIDS database as part of the pipeline step (default: true)"
         ),
     )
     model_config = ConfigDict(extra="forbid")
