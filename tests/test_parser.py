b9882cd8dbd09ed108cfcb090c69aaba57a967f8
Mathieu Dugré <16450132+mathdugre@users.noreply.github.com>
2024-10-03 13:18:30 -0400

    [API] make --dataset-root and positional arg. (#390)
    * [API] make --dataset-root and positional arg.
    
    * [MAINT] Only move PRs to pending review. (#389)
    
    * [MAINT] Only move PRs to pending review.
    
    The PR states is then handled by the project workflows.
    
    * cleanup file from removed workflow.
    
    * positional arg should not hae hyphen. Fixed to pass tests.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -26,7 +26,7 @@ from nipoppy.cli.parser import (
 def test_add_arg_dataset_root(dataset_root: str):
     parser = ArgumentParser()
     parser = add_arg_dataset_root(parser)
-    assert parser.parse_args(["--dataset-root", dataset_root])
+    assert parser.parse_args([dataset_root])
 
 
 def test_add_arg_version():
@@ -100,16 +100,16 @@ def test_add_subparser_init():
     parser = ArgumentParser()
     subparsers = parser.add_subparsers()
     add_subparser_init(subparsers)
-    assert parser.parse_args(["init", "--dataset-root", "my_dataset"])
+    assert parser.parse_args(["init", "my_dataset"])
 
 
 @pytest.mark.parametrize(
     "args",
     [
-        ["--dataset-root", "my_dataset"],
-        ["--dataset-root", "my_dataset", "--empty"],
-        ["--dataset-root", "my_dataset", "--regenerate"],
-        ["--dataset-root", "my_dataset", "--empty", "--regenerate"],
+        ["my_dataset"],
+        ["my_dataset", "--empty"],
+        ["my_dataset", "--regenerate"],
+        ["my_dataset", "--empty", "--regenerate"],
     ],
 )
 def test_add_subparser_doughnut(args):
@@ -122,9 +122,9 @@ def test_add_subparser_doughnut(args):
 @pytest.mark.parametrize(
     "args",
     [
-        ["--dataset-root", "my_dataset"],
-        ["--dataset-root", "my_dataset", "--copy-files"],
-        ["--dataset-root", "my_dataset", "--check-dicoms"],
+        ["my_dataset"],
+        ["my_dataset", "--copy-files"],
+        ["my_dataset", "--check-dicoms"],
     ],
 )
 def test_add_subparser_dicom_reorg(args):
@@ -137,10 +137,9 @@ def test_add_subparser_dicom_reorg(args):
 @pytest.mark.parametrize(
     "args",
     [
-        ["--dataset-root", "my_dataset", "--pipeline", "pipeline1"],
-        ["--dataset-root", "my_dataset", "--pipeline", "pipeline1", "--simulate"],
+        ["my_dataset", "--pipeline", "pipeline1"],
+        ["my_dataset", "--pipeline", "pipeline1", "--simulate"],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline1",
@@ -159,10 +158,9 @@ def test_add_subparser_bids_conversion(args):
 @pytest.mark.parametrize(
     "args",
     [
-        ["--dataset-root", "my_dataset", "--pipeline", "pipeline1"],
-        ["--dataset-root", "my_dataset", "--pipeline", "pipeline1", "--simulate"],
+        ["my_dataset", "--pipeline", "pipeline1"],
+        ["my_dataset", "--pipeline", "pipeline1", "--simulate"],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline1",
@@ -170,7 +168,6 @@ def test_add_subparser_bids_conversion(args):
             "1.2.3",
         ],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline1",
@@ -180,7 +177,6 @@ def test_add_subparser_bids_conversion(args):
             "step1",
         ],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline2",
@@ -188,7 +184,6 @@ def test_add_subparser_bids_conversion(args):
             "1000",
         ],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline2",
@@ -209,9 +204,8 @@ def test_add_subparser_pipeline_run(args):
 @pytest.mark.parametrize(
     "args",
     [
-        ["--dataset-root", "my_dataset", "--pipeline", "pipeline1"],
+        ["my_dataset", "--pipeline", "pipeline1"],
         [
-            "--dataset-root",
             "my_dataset",
             "--pipeline",
             "pipeline1",
@@ -234,13 +228,13 @@ def test_add_subparser_pipeline_track(args):
     [
         ["-h"],
         ["init", "-h"],
-        ["init", "--dataset-root", "my_dataset"],
-        ["init", "--dataset-root", "my_dataset", "--bids-source", "foo/bar"],
-        ["doughnut", "--dataset-root", "my_dataset", "--regenerate"],
-        ["reorg", "--dataset-root", "my_dataset", "--copy-files"],
-        ["bidsify", "--dataset-root", "my_dataset", "--pipeline", "a_bids_pipeline"],
-        ["run", "--dataset-root", "my_dataset", "--pipeline", "a_pipeline"],
-        ["track", "--dataset-root", "my_dataset", "--pipeline", "another_pipeline"],
+        ["init", "my_dataset"],
+        ["init", "my_dataset", "--bids-source", "foo/bar"],
+        ["doughnut", "my_dataset", "--regenerate"],
+        ["reorg", "my_dataset", "--copy-files"],
+        ["bidsify", "my_dataset", "--pipeline", "a_bids_pipeline"],
+        ["run", "my_dataset", "--pipeline", "a_pipeline"],
+        ["track", "my_dataset", "--pipeline", "another_pipeline"],
     ],
 )
 def test_global_parser(args: list[str]):
