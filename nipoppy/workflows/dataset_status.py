dc9f9d26b0842c78f476d074409cc41d619feb8f
nikhil153 <nikhil153@gmail.com>
2024-11-17 21:40:32 -0500

    refactored emoji logic for consistency

--- nipoppy/workflows/dataset_status.py
+++ nipoppy/workflows/dataset_status.py
@@ -49,18 +49,19 @@ class StatusWorkflow(BaseWorkflow):
         self.logger.info("Checking the status of the dataset.")
 
         # load global_config to get the dataset name
-        dataset_name = self.config.DATASET_NAME
-        self.logger.info(f"Dataset name: {dataset_name}")
+        # dataset_name = self.config.DATASET_NAME
+        # self.logger.info(f"Dataset name: {dataset_name}")
 
         status_df = pd.DataFrame()
 
         status_df = self._check_manifest(status_df)
         status_df = self._check_doughnut(status_df)
         status_df = self._check_bagel(status_df)
+        status_df, legend_dict = self._add_rewards(status_df)
 
         self.logger.debug(status_df)
 
-        self._df_to_table(status_df)
+        self._df_to_table(status_df, legend_dict)
 
         # TODO in future release
         # save the status to a file (probably needs a schema)
@@ -208,69 +209,104 @@ class StatusWorkflow(BaseWorkflow):
         status_df = pd.concat([status_df, bagel_status_df], axis=1)
         return status_df
 
-    def _df_to_table(self, status_df: pd.DataFrame):
-        """Convert a pandas.DataFrame obj into a rich.Table obj."""
-        df = status_df.copy()
-        df = df.fillna(0).astype(int).reset_index()
+    def _add_rewards(self, status_df: pd.DataFrame) -> pd.DataFrame:
+        """Add rewards column to the status DataFrame."""
+        status_df = status_df.fillna(0).astype(int).reset_index()
+        status_df = status_df.sort_values(by=self.manifest.col_session_id)
 
         # generate positive reinforcement emojis
-        crossed_fingers_emoji = "🤞"
-        rocket_emoji = "🚀"
+        clipboard = "📋"
+
         sad_cat_emoji = "😿"
+        broom_emoji = "🧹"
         doughnut_emoji = "🍩"
+        sparkles_emoji = "✨"
+        rocket_emoji = "🚀"
         bagel_emoji = "🥯"
-        # party_popper_emoji = "🎉"
-        sparkles = "✨"
+        party_popper_emoji = "🎉"
         confetti_emoji = "🎊"
+
+        # based on quantity
+        high_throughput = 100
+        bento_box_emoji = "🍱"  # 100 subjects in BIDS
+        mate_emoji = "🧉"  # 100 subjects processed
+
+        # secret rewards based on quantity
+        # shaved_ice_emoji = "🍧"
+        # sushi_emoji = "🍣"
+
+        # secret rewards based on update time and date
+        # night_owl_emoji = "🦉"
+        # slow_sloth_emoji = "🦥"
+        # quick_tiger_emoji = "🐅"
 
         # emoji legend
         legend_dict = {
             sad_cat_emoji: "No available data are curated or processed",
-            doughnut_emoji: "Data curation has started. Doughnut is made!",
-            sparkles: "BIDSification is complete! Tidiness sparks joy!",
-            bagel_emoji: "Data processing has started. Bagels are baking!",
-            rocket_emoji: "At least 1 pipeline processing is complete. "
-            "Successful launch!",
-            confetti_emoji: "All data are curated and processed. " "Time to celebrate!",
+            broom_emoji: "Data clean-up (i.e. BIDSification) has started!",
+            doughnut_emoji: "Curation in progress... Doughnut is made!",
+            sparkles_emoji: "BIDSification is complete! Tidiness sparks joy!",
+            rocket_emoji: "Data processing has launched successfully!",
+            bagel_emoji: "Data processing in progress... Bagels are baking!",
+            party_popper_emoji: "At least 1 pipeline processing is complete!",
+            confetti_emoji: "All data are curated and processed. Time to celebrate!",
         }
 
         doughnut_cols = ["in_pre_reorg", "in_post_reorg", "in_bids"]
-        bagel_cols = [col for col in df.columns if "\n" in col]
+        bagel_cols = [col for col in status_df.columns if "\n" in col]
         non_session_cols = ["in_manifest"] + doughnut_cols + bagel_cols
-        for row_obj in df.iterrows():
+        for row_obj in status_df.iterrows():
             row = row_obj[1]
 
             row_stickers = []
-            if (
-                (row["in_manifest"] > 0)
-                & (row[doughnut_cols] == 0).all()
-                & (row[bagel_cols] == 0).all()
-            ):
-                row_stickers.append(sad_cat_emoji)
+            if row["in_manifest"] > 0:
+                if (row[doughnut_cols] == 0).all() & (row[bagel_cols] == 0).all():
+                    row_stickers.append(sad_cat_emoji)
+                else:
+                    row_stickers.append(clipboard)
+
+            # curation tasks
             if (row[doughnut_cols] > 0).all():
                 row_stickers.append(doughnut_emoji)
             if row["in_bids"] == row["in_manifest"]:
-                row_stickers.append(sparkles)
+                row_stickers.append(sparkles_emoji)
+            elif (row["in_bids"] > 0) & (row["in_bids"] < row["in_manifest"]):
+                row_stickers.append(broom_emoji)
+
+            if row["in_bids"] > high_throughput:  # hard to test
+                row_stickers.append(bento_box_emoji)
+
+            # processing tasks
             if (row[bagel_cols] > 0).all():
                 row_stickers.append(bagel_emoji)
-            if (row[bagel_cols] == row["in_manifest"]).any():
-                row_stickers.append(rocket_emoji)
+
+            # check if processing is complete for all participants and pipelines
             if (row[non_session_cols] > 0).all():
                 row_vals = row[non_session_cols].values
                 all_equal = all([x == row_vals[0] for x in row_vals])
                 if all_equal:
                     row_stickers.append(confetti_emoji)
 
-            df.loc[row_obj[0], "milestones"] = " ".join(row_stickers)
+            # Cannot nest this in prior if statement because
+            # some pipeline count might be zero
+            if (not all_equal) & (row[bagel_cols] == row["in_manifest"]).any():
+                row_stickers.append(party_popper_emoji)
+            elif (row[bagel_cols] > 0).any():
+                row_stickers.append(rocket_emoji)
 
+            if (row[bagel_cols] > high_throughput).any():  # hard to test
+                row_stickers.append(mate_emoji)
+
+            status_df.loc[row_obj[0], "rewards"] = " ".join(row_stickers)
+
+        return status_df, legend_dict
+
+    def _df_to_table(self, status_df: pd.DataFrame, legend_dict: dict):
+        """Convert a pandas.DataFrame obj into a rich.Table obj."""
+        df = status_df.copy()
         console = Console()
 
-        # Initiate a Table instance
-        title = (
-            "Participant counts by session at each Nipoppy checkpoint"
-            + f" {crossed_fingers_emoji} {crossed_fingers_emoji}"
-        )  # + f"\n{legend_dict}"
-
+        crossed_fingers_emoji = "🤞"
         column_colors = [
             "cyan",
             "magenta",
@@ -280,6 +316,12 @@ class StatusWorkflow(BaseWorkflow):
             "deep_pink2",
             "rosy_brown",
         ]
+
+        # Initiate a Table instance
+        title = (
+            "Participant counts by session at each Nipoppy checkpoint"
+            + f" {crossed_fingers_emoji} {crossed_fingers_emoji}"
+        )
 
         table = Table(title=title, collapse_padding=False)
 
--- tests/test_workflow_dataset_status.py
+++ tests/test_workflow_dataset_status.py
@@ -9,6 +9,8 @@ from nipoppy.tabular.doughnut import Doughnut
 from nipoppy.tabular.manifest import Manifest
 from nipoppy.workflows.dataset_status import StatusWorkflow
 
+from .conftest import get_config
+
 
 @pytest.fixture(params=["my_dataset", "dataset_dir"])
 def dpath_root(request: pytest.FixtureRequest, tmp_path: Path) -> Path:
@@ -18,6 +20,8 @@ def dpath_root(request: pytest.FixtureRequest, tmp_path: Path) -> Path:
 def test_run(dpath_root: Path):
 
     workflow = StatusWorkflow(dpath_root=dpath_root)
+
+    workflow.config = get_config(dataset_name="my_dataset")
 
     manifest_dict = {
         Manifest.col_participant_id: ["01", "02", "03", "01", "02"],
@@ -87,8 +91,9 @@ def test_run(dpath_root: Path):
     workflow.bagel = Bagel.from_dict(bagel_dict)
 
     status_df = workflow.run_main()
-    status_df = status_df.fillna(0).astype(int).reset_index()
+    # status_df = status_df.fillna(0).astype(int).reset_index()
 
+    print(status_df)
     # check manifest status
     assert set(status_df[Manifest.col_session_id].unique()) == set(["BL", "M12"])
     assert status_df[Manifest.col_session_id].nunique() == 2
@@ -102,3 +107,11 @@ def test_run(dpath_root: Path):
     # check bagel status
     assert status_df["dcm2bids\n1.0.0\nconvert"].sum() == 2
     assert status_df["fmriprep\n2.0.0\ndefault"].sum() == 1
+
+    # check emoji status
+    test_stickers = ["📋", "🍩", "🧹", "🥯", "🚀"]
+    test_rewards = " ".join(test_stickers)
+    calculated_rewards = status_df[status_df[Manifest.col_session_id] == "BL"][
+        "rewards"
+    ].values.astype(str)
+    assert calculated_rewards == test_rewards
