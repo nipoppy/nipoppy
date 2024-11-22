64e5f7baaa91ffb29718b879ef7608c42765ca13
jacobsanz97 <jacobsanz97@hotmail.com>
2024-11-21 21:13:17 -0500

    testing different parallel environments in SGE

---
+++ nipoppy/data/hpc_templates/sge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N {{job_name}}
+#$ -wd {{working_directory}}
+{%- if cores %}
+#$ -pe all.pe {{cores}}
+{%- endif %}
+{%- if memory_max %}
+#$ -l h_vmem={{memory_max}}
+{%- endif %}
+{%- if run_time_max %}
+#$ -l h_rt={{ '%02d:%02d:%02d' % (run_time_max // 60, run_time_max % 60, 0) }}
+{%- endif %}
+{%- if account %}
+#$ -q {{account}}
+{%- endif %}
+#$ -o time.out
+#$ -e error.out
+
+{{command}}
+
