c50be54212e9ae199a2fd62e516298ec123f2db8
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2025-01-27 20:27:35 -0500

    add --mode to parser

--- nipoppy/cli/parser.py
+++ nipoppy/cli/parser.py
@@ -174,6 +174,15 @@ def add_subparser_init(
             "with. Note: this will automatically create an appropriate manifest file."
         ),
     )
+    parser.add_argument(
+        "--mode",
+        type=str,
+        required=False,
+        help=(
+            "If using a BIDS source, specify whether to copy or move the files "
+            "(default: copy)."
+        ),
+    )
 
     return parser
 
