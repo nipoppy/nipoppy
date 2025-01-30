9a7f0379d9647a26b9401a74609adfb876aeb425
Michelle Wang <tomichellewang@gmail.com>
2025-01-30 17:19:41 -0500

    update test

--- tests/test_workflow_init.py
+++ tests/test_workflow_init.py
@@ -204,18 +204,14 @@ def test_init_bids_symlink_mode(tmp_path):
     source_files_after_init = [
         x.relative_to(bids_to_link) for x in bids_to_link.glob("**/*")
     ]
-    target_files = [
-        x.relative_to(dpath_root / "bids") for x in dpath_root.glob("bids/**/*")
-    ]
 
     for f in source_files_before_init:
-        assert f in target_files
-
-    for f in source_files_after_init:
-        assert f in target_files
+        assert f in source_files_after_init
 
     assert (dpath_root / "bids").is_symlink()
     # only the directory is linked, not the files within
+    
+    assert (dpath_root / "bids").readlink() == bids_to_link
 
     assert len(source_files_after_init) == 25
     assert (dpath_root / "bids" / "README.md").exists()
