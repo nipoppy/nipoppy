57d2ffec2cde0e115c5b4f5d3dd8eb79b6da42db
Michelle Wang <tomichellewang@gmail.com>
2025-01-30 10:41:46 -0500

    [ENH] Update Freesurfer stats/QC extractor (#464)
    * bump version to 0.1.1
    
    * add explicit arguments for parcellation and aparc/aseg measure
    
    * auto-generate output file names
    
    * split aparc and aseg files
    
    * update descriptor and invocation
    
    * add requirements.txt (not sure if this is the best way)
    
    * rename to version 0.2.0
    
    * iterate over multiple aparc/aseg measures + include FS version in output filenames
    
    * rename to "fs_stats"
    
    * rename to "fs_stats"
    
    * update helptext for optional args (--subjects, --tablefile)

--- tests/test_default_config.py
+++ tests/test_default_config.py
@@ -357,7 +357,7 @@ def test_tracker_paths(
 @pytest.mark.parametrize(
     "pipeline_name,pipeline_version",
     [
-        ("freesurfer_stats_and_qc", "0.1.0"),
+        ("fs_stats", "0.2.0"),
     ],
 )
 def test_extractor(
