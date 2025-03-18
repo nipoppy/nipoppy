df3c8d10918c9acdb91fcaa3b086ee4166f97359
Michelle Wang <tomichellewang@gmail.com>
2025-03-18 16:13:22 -0400

    clean up DoughnutWorkflow tests

--- tests/test_workflow_doughnut.py
+++ tests/test_workflow_doughnut.py
@@ -6,17 +6,19 @@ import pytest
 
 from nipoppy.tabular.doughnut import Doughnut
 from nipoppy.tabular.manifest import Manifest
-from nipoppy.utils import save_json
 from nipoppy.workflows.doughnut import DoughnutWorkflow
 
-from .conftest import (
-    ATTR_TO_DPATH_MAP,
-    ATTR_TO_FPATH_MAP,
-    check_doughnut,
-    create_empty_dataset,
-    get_config,
-    prepare_dataset,
-)
+from .conftest import check_doughnut, create_empty_dataset, get_config, prepare_dataset
+
+
+@pytest.fixture(scope="function")
+def workflow(tmp_path: Path):
+    dpath_root = tmp_path / "my_dataset"
+    create_empty_dataset(dpath_root)
+    workflow = DoughnutWorkflow(dpath_root=dpath_root)
+    workflow.config = get_config()
+    workflow.config.save(workflow.layout.fpath_config)
+    return workflow
 
 
 @pytest.mark.parametrize(
@@ -50,42 +52,31 @@ from .conftest import (
 )
 @pytest.mark.parametrize("empty", [True, False])
 def test_run_main(
+    workflow: DoughnutWorkflow,
     participants_and_sessions_manifest1: dict[str, list[str]],
     participants_and_sessions_manifest2: dict[str, list[str]],
     participants_and_sessions_downloaded: dict[str, list[str]],
     participants_and_sessions_organized: dict[str, list[str]],
     participants_and_sessions_bidsified: dict[str, list[str]],
     empty: bool,
-    tmp_path: Path,
 ):
-    dpath_root = tmp_path / "my_dataset"
+    workflow.empty = empty
 
-    dpath_downloaded = dpath_root / ATTR_TO_DPATH_MAP["dpath_pre_reorg"]
-    dpath_organized = dpath_root / ATTR_TO_DPATH_MAP["dpath_post_reorg"]
-    dpath_bidsified = dpath_root / ATTR_TO_DPATH_MAP["dpath_bids"]
-    fpath_manifest = dpath_root / ATTR_TO_FPATH_MAP["fpath_manifest"]
-    fpath_config = dpath_root / ATTR_TO_FPATH_MAP["fpath_config"]
-    fpath_doughnut = dpath_root / ATTR_TO_FPATH_MAP["fpath_doughnut"]
-
-    create_empty_dataset(dpath_root)
+    # initial manifest
     manifest1 = prepare_dataset(
         participants_and_sessions_manifest=participants_and_sessions_manifest1,
         participants_and_sessions_downloaded=participants_and_sessions_downloaded,
         participants_and_sessions_organized=participants_and_sessions_organized,
         participants_and_sessions_bidsified=participants_and_sessions_bidsified,
-        dpath_downloaded=dpath_downloaded,
-        dpath_organized=dpath_organized,
-        dpath_bidsified=dpath_bidsified,
+        dpath_downloaded=workflow.layout.dpath_pre_reorg,
+        dpath_organized=workflow.layout.dpath_post_reorg,
+        dpath_bidsified=workflow.layout.dpath_bids,
     )
-    manifest1.save_with_backup(fpath_manifest)
-
-    # prepare config file
-    config = get_config()
-    save_json(config.model_dump(mode="json"), fpath_config)
+    workflow.manifest = manifest1
 
     # generate the doughnut
-    DoughnutWorkflow(dpath_root=dpath_root, empty=empty).run_main()
-    doughnut1 = Doughnut.load(fpath_doughnut)
+    workflow.run_main()
+    doughnut1 = Doughnut.load(workflow.layout.fpath_doughnut)
 
     assert len(doughnut1) == len(manifest1)
     check_doughnut(
@@ -97,13 +88,13 @@ def test_run_main(
         empty=empty,
     )
 
-    # update the manifest
+    # update the manifest (add rows)
     manifest2 = prepare_dataset(participants_and_sessions_manifest2)
-    manifest2.save_with_backup(fpath_manifest)
+    manifest2.save_with_backup(workflow.layout.fpath_manifest)
 
     # update the doughnut
-    DoughnutWorkflow(dpath_root=dpath_root, empty=empty).run()
-    doughnut2 = Doughnut.load(fpath_doughnut)
+    DoughnutWorkflow(dpath_root=workflow.dpath_root, empty=empty).run_main()
+    doughnut2 = Doughnut.load(workflow.layout.fpath_doughnut)
 
     assert len(doughnut2) == len(manifest2)
     check_doughnut(
@@ -140,37 +131,26 @@ def test_run_main(
 )
 @pytest.mark.parametrize("empty", [True, False])
 def test_run_main_regenerate(
+    workflow: DoughnutWorkflow,
     participants_and_sessions_manifest: dict[str, list[str]],
     participants_and_sessions_downloaded: dict[str, list[str]],
     participants_and_sessions_organized: dict[str, list[str]],
     participants_and_sessions_bidsified: dict[str, list[str]],
     empty: bool,
-    tmp_path: Path,
 ):
-    dpath_root = tmp_path / "my_dataset"
-    create_empty_dataset(dpath_root)
-
-    dpath_downloaded = dpath_root / ATTR_TO_DPATH_MAP["dpath_pre_reorg"]
-    dpath_organized = dpath_root / ATTR_TO_DPATH_MAP["dpath_post_reorg"]
-    dpath_bidsified = dpath_root / ATTR_TO_DPATH_MAP["dpath_bids"]
-    fpath_manifest = dpath_root / ATTR_TO_FPATH_MAP["fpath_manifest"]
-    fpath_config = dpath_root / ATTR_TO_FPATH_MAP["fpath_config"]
-    fpath_doughnut = dpath_root / ATTR_TO_FPATH_MAP["fpath_doughnut"]
+    workflow.empty = empty
+    workflow.regenerate = True
 
     manifest = prepare_dataset(
         participants_and_sessions_manifest=participants_and_sessions_manifest,
         participants_and_sessions_downloaded=participants_and_sessions_downloaded,
         participants_and_sessions_organized=participants_and_sessions_organized,
         participants_and_sessions_bidsified=participants_and_sessions_bidsified,
-        dpath_downloaded=dpath_downloaded,
-        dpath_organized=dpath_organized,
-        dpath_bidsified=dpath_bidsified,
+        dpath_downloaded=workflow.layout.dpath_pre_reorg,
+        dpath_organized=workflow.layout.dpath_post_reorg,
+        dpath_bidsified=workflow.layout.dpath_bids,
     )
-    manifest.save_with_backup(fpath_manifest)
-
-    # prepare config file
-    config = get_config()
-    save_json(config.model_dump(mode="json"), fpath_config)
+    workflow.manifest = manifest
 
     # to be overwritten
     doughnut_records = []
@@ -189,11 +169,11 @@ def test_run_main_regenerate(
             }
         )
     doughnut_old = Doughnut(doughnut_records)
-    assert doughnut_old.save_with_backup(fpath_doughnut) is not None
+    assert doughnut_old.save_with_backup(workflow.layout.fpath_doughnut) is not None
 
     # regenerate the doughnut
-    DoughnutWorkflow(dpath_root=dpath_root, empty=empty, regenerate=True).run_main()
-    doughnut = Doughnut.load(fpath_doughnut)
+    workflow.run_main()
+    doughnut = Doughnut.load(workflow.layout.fpath_doughnut)
 
     assert len(doughnut) == len(manifest)
     check_doughnut(
