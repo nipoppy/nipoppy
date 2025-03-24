8d3fa253893e141fa9a85777e0b92508dde2d922
Michelle Wang <tomichellewang@gmail.com>
2025-03-24 13:40:24 -0400

    add base "pipeline install" functionality

---
+++ nipoppy/workflows/pipeline_store/install.py
@@ -0,0 +1,68 @@
+"""Workflow for pipeline install command."""
+
+import logging
+from pathlib import Path
+from typing import Optional
+
+from nipoppy.env import LogColor, StrOrPathLike
+from nipoppy.pipeline_store.validation import check_pipeline_bundle
+from nipoppy.workflows.base import BaseWorkflow
+
+
+class PipelineInstallWorkflow(BaseWorkflow):
+    """Workflow for pipeline install command."""
+
+    def __init__(
+        self,
+        dpath_root: Path,
+        dpath_pipeline: StrOrPathLike,
+        overwrite: bool = False,
+        fpath_layout: Optional[StrOrPathLike] = None,
+        verbose: bool = False,
+        dry_run: bool = False,
+    ):
+        """Initialize the workflow."""
+        super().__init__(
+            dpath_root=dpath_root,
+            name="pipeline_install",
+            fpath_layout=fpath_layout,
+            verbose=verbose,
+            dry_run=dry_run,
+        )
+        self.dpath_pipeline = dpath_pipeline
+        self.overwrite = overwrite
+
+    def run_main(self):
+        """Install a pipeline config directory into the dataset."""
+        # load the config and validate file contents (including file paths)
+        pipeline_config = check_pipeline_bundle(self.dpath_pipeline, logger=self.logger)
+
+        # generate destination path
+        dpath_target = self.layout.get_dpath_pipeline_bundle(
+            pipeline_config.PIPELINE_TYPE, pipeline_config.NAME, pipeline_config.VERSION
+        )
+
+        # check if the target directory already exists
+        if dpath_target.exists():
+            if not self.overwrite:
+                raise FileExistsError(
+                    f"Pipeline directory exists: {dpath_target}"
+                    ". Use --overwrite to overwrite.",
+                )
+            else:
+                self.logger.info(
+                    f"Overwriting existing pipeline directory: {dpath_target}",
+                )
+                self.rm(dpath_target, log_level=logging.DEBUG)
+
+        # copy the directory
+        self.copytree(
+            path_source=self.dpath_pipeline,
+            path_dest=dpath_target,
+        )
+
+        self.logger.info(
+            f"[{LogColor.SUCCESS}]Successfully installed pipeline "
+            f"{pipeline_config.NAME}, version {pipeline_config.VERSION} "
+            f"at {dpath_target}![/]"
+        )
---
+++ tests/test_workflow_pipeline_store_install.py
@@ -0,0 +1,86 @@
+"""Tests for PipelineInstallWorkflow class."""
+
+from contextlib import nullcontext
+from pathlib import Path
+
+import pytest
+
+from nipoppy.env import PipelineTypeEnum
+from nipoppy.workflows.pipeline_store.install import PipelineInstallWorkflow
+
+from .conftest import create_empty_dataset, create_pipeline_config_files
+
+
+@pytest.fixture()
+def pipeline_config():
+    return {
+        "NAME": "my_pipeline",
+        "VERSION": "1.0.0",
+        "PIPELINE_TYPE": PipelineTypeEnum.PROCESSING,
+    }
+
+
+@pytest.fixture(scope="function")
+def workflow(tmp_path: Path, pipeline_config: dict):
+    dpath_root = tmp_path / "my_dataset"
+    create_empty_dataset(dpath_root)
+    create_pipeline_config_files(
+        tmp_path,
+        proc_pipelines=[pipeline_config],
+    )
+    workflow = PipelineInstallWorkflow(
+        dpath_root=dpath_root,
+        dpath_pipeline=tmp_path / "proc" / "my_pipeline-1.0.0",
+    )
+    return workflow
+
+
+def _check_files_copied(dpath_source, dpath_dest):
+    paths_source = set(
+        path.relative_to(dpath_source) for path in dpath_source.rglob("*")
+    )
+    paths_dest = set(path.relative_to(dpath_dest) for path in dpath_dest.rglob("*"))
+    assert paths_source == paths_dest
+
+
+def test_run_main(
+    workflow: PipelineInstallWorkflow,
+    pipeline_config: dict,
+    caplog: pytest.LogCaptureFixture,
+):
+    dpath_installed = workflow.layout.get_dpath_pipeline_bundle(
+        pipeline_config["PIPELINE_TYPE"],
+        pipeline_config["NAME"],
+        pipeline_config["VERSION"],
+    )
+    assert not dpath_installed.exists()
+
+    workflow.run_main()
+    _check_files_copied(
+        workflow.dpath_pipeline,
+        dpath_installed,
+    )
+    assert "Successfully installed pipeline" in caplog.text
+
+
+@pytest.mark.parametrize("overwrite", [False, True])
+def test_run_main_overwrite(
+    workflow: PipelineInstallWorkflow, pipeline_config: dict, overwrite: bool
+):
+    workflow.overwrite = overwrite
+
+    # create directory where the pipeline is supposed to be installed
+    dpath_installed = workflow.layout.get_dpath_pipeline_bundle(
+        pipeline_config["PIPELINE_TYPE"],
+        pipeline_config["NAME"],
+        pipeline_config["VERSION"],
+    )
+    dpath_installed.mkdir(parents=True)
+
+    with (
+        nullcontext()
+        if overwrite
+        else pytest.raises(FileExistsError, match="Use --overwrite to overwrite")
+    ):
+        workflow.run_main()
+        _check_files_copied(workflow.dpath_pipeline, dpath_installed)
