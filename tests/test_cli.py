34a5ddd25174b46ae114600fc45b170afb07e871
Michelle Wang <tomichellewang@gmail.com>
2025-03-28 11:15:59 -0400

    fix CLI test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,9 @@
 """Tests for the CLI."""
 
+import logging
 from pathlib import Path
 
+import pytest
 from click.testing import CliRunner
 
 from nipoppy.cli import cli
@@ -16,6 +18,7 @@ def test_cli():
     result = runner.invoke(
         cli,
         ["-h"],
+        catch_exceptions=False,
     )
     assert result.exit_code == ReturnCode.SUCCESS
 
@@ -24,17 +27,27 @@ def test_cli_invalid():
     result = runner.invoke(
         cli,
         ["--fake-arg"],
+        catch_exceptions=False,
     )
     assert result.exit_code != ReturnCode.SUCCESS
 
 
-def test_dep_params(tmp_path: Path):
+def test_dep_params(tmp_path: Path, caplog: pytest.LogCaptureFixture):
     dpath_root = tmp_path / "my_dataset"
     result = runner.invoke(
         cli,
-        ["init", dpath_root],
+        ["init", str(dpath_root)],
+        catch_exceptions=False,
     )
-    assert result.exit_code != ReturnCode.SUCCESS
+
+    assert any(
+        [
+            "Giving the dataset path without --dataset is deprecated" in record.message
+            and record.levelno == logging.WARNING
+            for record in caplog.records
+        ]
+    )
+    assert result.exit_code == ReturnCode.SUCCESS
 
 
 def test_cli_init(tmp_path: Path):
@@ -42,6 +55,7 @@ def test_cli_init(tmp_path: Path):
     result = runner.invoke(
         cli,
         ["init", "--dataset", dpath_root],
+        catch_exceptions=False,
     )
     assert result.exit_code == ReturnCode.SUCCESS
 
@@ -51,6 +65,7 @@ def test_cli_status(tmp_path: Path):
     result = runner.invoke(
         cli,
         ["status", "--dataset", dpath_root],
+        catch_exceptions=False,
     )
 
     # No log file is created, since the status command does not create logs.
@@ -62,7 +77,9 @@ def test_cli_status(tmp_path: Path):
 
 def test_cli_doughnut(tmp_path: Path):
     dpath_root = tmp_path / "my_dataset"
-    result = runner.invoke(cli, ["doughnut", "--dataset", dpath_root])
+    result = runner.invoke(
+        cli, ["doughnut", "--dataset", dpath_root], catch_exceptions=False
+    )
 
     # check that a logfile was created
     assert (
@@ -76,7 +93,11 @@ def test_cli_doughnut(tmp_path: Path):
 
 def test_cli_reorg(tmp_path: Path):
     dpath_root = tmp_path / "my_dataset"
-    result = runner.invoke(cli, ["reorg", "--dataset", dpath_root])
+    result = runner.invoke(
+        cli,
+        ["reorg", "--dataset", dpath_root],
+        catch_exceptions=False,
+    )
 
     # check that a logfile was created
     assert (
@@ -165,12 +186,13 @@ def test_cli_track(tmp_path: Path):
         [
             "track",
             "--dataset",
-            dpath_root,
+            str(dpath_root),
             "--pipeline",
             "my_pipeline",
             "--pipeline-version",
             "1.0",
         ],
+        catch_exceptions=False,
     )
 
     # check that a logfile was created
@@ -196,12 +218,13 @@ def test_cli_extract(tmp_path: Path):
         [
             "extract",
             "--dataset",
-            dpath_root,
+            str(dpath_root),
             "--pipeline",
             "my_pipeline",
             "--pipeline-version",
             "1.0",
         ],
+        catch_exceptions=False,
     )
 
     # check that a logfile was created
@@ -225,7 +248,8 @@ def test_cli_pipeline_validate(tmp_path: Path):
     dpath_pipeline.mkdir()
     result = runner.invoke(
         cli,
-        ["pipeline", "validate", dpath_pipeline],
+        ["pipeline", "validate", str(dpath_pipeline)],
+        catch_exceptions=False,
     )
 
     # Expects missing path, since init command is not run.
