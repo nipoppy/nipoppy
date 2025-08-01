ae4cc2e4b3171b9ec51921902880fe793d55627d
mathdugre <16450132+mathdugre@users.noreply.github.com>
2025-07-31 21:28:32 -0400

    [TST] Improve coverage of pipeline upload workflow.

--- nipoppy/workflows/pipeline_store/upload.py
+++ nipoppy/workflows/pipeline_store/upload.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 from nipoppy.config.pipeline import BasePipelineConfig
 from nipoppy.console import CONSOLE_STDOUT
-from nipoppy.env import LogColor, StrOrPathLike
+from nipoppy.env import LogColor, ReturnCode, StrOrPathLike
 from nipoppy.pipeline_validation import check_pipeline_bundle
 from nipoppy.utils import get_today, load_json
 from nipoppy.workflows.base import BaseWorkflow
@@ -90,7 +90,7 @@ class ZenodoUploadWorkflow(BaseWorkflow):
             self.logger.error(
                 f"Pipeline validation failed. Please check the pipeline files: {e}"
             )
-            raise SystemExit(1)
+            raise SystemExit(ReturnCode.UNKNOWN_FAILURE)
 
         if self.record_id:
             self.record_id = self.record_id.removeprefix("zenodo.")
