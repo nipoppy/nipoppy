b05cdf7113c9a1444ceb56e099709ba105f21c39
Mathieu Dugré <16450132+mathdugre@users.noreply.github.com>
2025-08-13 13:38:50 -0400

    Update nipoppy/config/pipeline_step.py
    Co-authored-by: Michelle Wang <tomichellewang@gmail.com>

--- nipoppy/config/pipeline_step.py
+++ nipoppy/config/pipeline_step.py
@@ -48,7 +48,7 @@ class BasePipelineStepConfig(_SchemaWithContainerConfig, ABC):
     )
     DESCRIPTOR_FILE: Optional[Path] = Field(
         default=None,
-        description=("Path to the JSON descriptor file."),
+        description="Path to the JSON descriptor file.",
     )
     INVOCATION_FILE: Optional[Path] = Field(
         default=None,
