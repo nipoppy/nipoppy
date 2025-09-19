5d3a171915951474a77a857429195cfe09d02525
Michelle Wang <tomichellewang@gmail.com>
2025-09-19 11:00:24 -0400

    [ENH] Do not overwrite existing pipeline variables when installing a pipeline (#760)
    * add failing test
    
    * do not overwrite existing variables
    
    * do not log warning messages if no variables were updated
    
    * make test less confusing
    
    * use new class name

--- tests/unit/workflows/pipeline_store/test_install.py
+++ tests/unit/workflows/pipeline_store/test_install.py
@@ -187,6 +187,37 @@ def test_update_config_and_save_no_other_change(
     ) == original_config.model_dump(exclude="PIPELINE_VARIABLES")
 
 
+def test_update_config_and_save_no_overwrite(
+    workflow: PipelineInstallWorkflow,
+    pipeline_config: ProcessingPipelineConfig,
+    caplog: pytest.LogCaptureFixture,
+):
+    variable_name = "var1"
+    variable_value = "some_value"
+    pipeline_config.VARIABLES = {
+        variable_name: "this is a variable that is important for the pipeline",
+    }
+    workflow.config.PIPELINE_VARIABLES.set_variables(
+        pipeline_config.PIPELINE_TYPE,
+        pipeline_config.NAME,
+        pipeline_config.VERSION,
+        {variable_name: variable_value},
+    )
+    workflow.config.save(workflow.layout.fpath_config)
+
+    workflow._update_config_and_save(pipeline_config)
+
+    updated_config = workflow.config.load(workflow.layout.fpath_config)
+    assert updated_config.PIPELINE_VARIABLES.get_variables(
+        pipeline_config.PIPELINE_TYPE,
+        pipeline_config.NAME,
+        pipeline_config.VERSION,
+    ) == {variable_name: variable_value}
+
+    # should not log any warnings about adding variables
+    assert not any([record.levelno == logging.WARNING for record in caplog.records])
+
+
 def test_download_container(
     workflow: PipelineInstallWorkflow,
     pipeline_config: ProcessingPipelineConfig,
