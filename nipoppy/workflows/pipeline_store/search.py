dff56776e4a8f6ba5f9800db5aa5da43ccd1599e
Michelle Wang <tomichellewang@gmail.com>
2025-09-23 14:51:07 -0400

    download all results internally; add DOI link to table

--- nipoppy/workflows/pipeline_store/search.py
+++ nipoppy/workflows/pipeline_store/search.py
@@ -15,6 +15,8 @@ from nipoppy.zenodo_api import ZenodoAPI
 class PipelineSearchWorkflow(BaseWorkflow):
     """Search Zenodo for existing pipeline configurations and print results table."""
 
+    _api_search_size = int(1e9)
+
     def __init__(
         self,
         query: str,
@@ -40,13 +42,17 @@ class PipelineSearchWorkflow(BaseWorkflow):
         for hit in hits:
             data_for_df.append(
                 {
-                    "Zenodo ID": hit.get("id"),
+                    "Zenodo ID": f'[link={hit.get("doi_url")}]{hit.get("id")}[/link]',
                     "Title": hit.get("title"),
                     "Description": hit.get("metadata", {}).get("description"),
                     "Downloads": hit.get("stats", {}).get("downloads"),
                 }
             )
-        return pd.DataFrame(data_for_df).sort_values("Downloads", ascending=False)
+        return (
+            pd.DataFrame(data_for_df)
+            .sort_values("Downloads", ascending=False)
+            .iloc[: self.size]
+        )
 
     def _df_to_table(self, df_hits: pd.DataFrame) -> Table:
         table = Table(box=box.MINIMAL_DOUBLE_HEAD)
@@ -59,10 +65,12 @@ class PipelineSearchWorkflow(BaseWorkflow):
     def run_main(self):
         """Run the workflow."""
         with CONSOLE_STDOUT.status("Searching Nipoppy pipelines on Zenodo..."):
+            # we get all results and sort/slice them ourselves since we cannot currently
+            # sort by "mostdownloaded" through the API
             results = self.zenodo_api.search_records(
                 query=self.query,
                 keywords=["Nipoppy"],
-                size=self.size,
+                size=self._api_search_size,
             )
         hits = results["hits"]
         n_total = results["total"]
@@ -76,5 +84,9 @@ class PipelineSearchWorkflow(BaseWorkflow):
         df_hits = self._hits_to_df(hits)
         table = self._df_to_table(df_hits)
 
-        self.logger.info(f"Showing {len(hits)} of {n_total} results")
+        n_shown = min(len(hits), self.size)
+        message = f"Showing {min(len(hits), self.size)} of {n_total} results"
+        if n_shown < n_total:
+            message += " (use --size to show more)"
+        self.logger.info(message)
         CONSOLE_STDOUT.print(table)
--- tests/unit/workflows/pipeline_store/test_search.py
+++ tests/unit/workflows/pipeline_store/test_search.py
@@ -32,27 +32,30 @@ def hits():
             "metadata": {
                 "description": "Description 1",
             },
+            "doi_url": "fake_doi_url_12345",
         },
         {
             "id": 67890,
             "title": "Pipeline 2",
             "stats": {"downloads": 100},
             "metadata": {},
+            "doi_url": "fake_doi_url_67890",
         },
     ]
 
 
 def test_hits_to_df(workflow: PipelineSearchWorkflow, hits: list[dict]):
+    workflow.size = len(hits)
     df = workflow._hits_to_df(hits)
-    assert len(df) == len(hits)
+    assert len(df) == workflow.size
 
     # order is switched because of sorting by downloads
-    assert df.iloc[0]["Zenodo ID"] == 67890
+    assert df.iloc[0]["Zenodo ID"] == "[link=fake_doi_url_67890]67890[/link]"
     assert df.iloc[0]["Title"] == "Pipeline 2"
     assert df.iloc[0]["Description"] is None
     assert df.iloc[0]["Downloads"] == 100
 
-    assert df.iloc[1]["Zenodo ID"] == 12345
+    assert df.iloc[1]["Zenodo ID"] == "[link=fake_doi_url_12345]12345[/link]"
     assert df.iloc[1]["Title"] == "Pipeline 1"
     assert df.iloc[1]["Description"] == "Description 1"
     assert df.iloc[1]["Downloads"] == 4
@@ -87,11 +90,12 @@ def test_run_main(
     workflow.run()
 
     workflow.zenodo_api.search_records.assert_called_once_with(
-        query=workflow.query, keywords=["Nipoppy"], size=workflow.size
+        query=workflow.query, keywords=["Nipoppy"], size=workflow._api_search_size
     )
     mocked_hits_to_df.assert_called_once_with(hits)
     mocked_df_to_table.assert_called_once_with(df)
-    assert "Showing 2 of 2 results" in caplog.text
+    assert "Showing 1 of 2 results" in caplog.text
+    assert " (use --size to show more)" in caplog.text
     mocked_console_status.assert_called_once()
 
 
@@ -112,3 +116,18 @@ def test_run_main_no_results(
             for record in caplog.records
         ]
     )
+
+
+def test_run_main_all_results(
+    workflow: PipelineSearchWorkflow,
+    hits: list[dict],
+    caplog: pytest.LogCaptureFixture,
+):
+    workflow.size = len(hits)
+
+    # mock search results
+    workflow.zenodo_api.search_records.return_value = {"hits": hits, "total": len(hits)}
+
+    workflow.run()
+
+    assert "(use --size to show more)" not in caplog.text
