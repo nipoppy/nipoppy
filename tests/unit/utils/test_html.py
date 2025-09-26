eed34eccbe9e4209c492f1b35bc5ad799e9e091c
Michelle Wang <tomichellewang@gmail.com>
2025-09-26 10:40:19 -0400

    cover last line?

--- tests/unit/utils/test_html.py
+++ tests/unit/utils/test_html.py
@@ -9,7 +9,7 @@ from nipoppy.utils.html import strip_html_tags
     "html,tags,expected",
     [
         ("<div><p>Test<br/></p></div>", None, "Test"),
-        ("<div><p>Test<keep></p></div>", None, "Test<keep>"),
+        ("<div><p>Test<keep/></p></div>", None, "Test<keep/>"),
         ("<div><p>Test</p></div>", {"p"}, "<div>Test</div>"),
     ],
 )
