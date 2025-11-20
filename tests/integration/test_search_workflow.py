656b70ac00945c2b70680974acd586c5cf599fee
mathdugre <16450132+mathdugre@users.noreply.github.com>
2025-11-20 12:17:16 -0500

    Use workflow in integration instead of API

--- tests/integration/test_search_workflow.py
+++ tests/integration/test_search_workflow.py
@@ -3,8 +3,6 @@ import pytest
 from nipoppy.workflows.pipeline_store.search import PipelineSearchWorkflow
 from nipoppy.zenodo_api import ZenodoAPI, ZenodoAPIError
 
-MAX_ZENODO_SEARCH_SIZE = PipelineSearchWorkflow._api_search_size
-
 
 @pytest.fixture
 def zenodo_api() -> ZenodoAPI:
@@ -13,13 +11,21 @@ def zenodo_api() -> ZenodoAPI:
 
 @pytest.mark.api
 def test_search_size(zenodo_api: ZenodoAPI):
-    zenodo_api.search_records("", size=MAX_ZENODO_SEARCH_SIZE)
+    PipelineSearchWorkflow(
+        query="",
+        zenodo_api=zenodo_api,
+    ).run()
 
 
 @pytest.mark.api
 def test_search_size_exceeds_max(zenodo_api: ZenodoAPI):
+    workflow = PipelineSearchWorkflow(
+        query="",
+        zenodo_api=zenodo_api,
+    )
+    workflow._api_search_size += 1
     with pytest.raises(
         ZenodoAPIError,
         match="Failed to search records. JSON response:",
     ):
-        zenodo_api.search_records("", size=MAX_ZENODO_SEARCH_SIZE + 1)
+        workflow.run()
