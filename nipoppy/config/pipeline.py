93536069efd65eea9657144e6c77dea95791b262
Mathieu Dugré <16450132+mathdugre@users.noreply.github.com>
2026-08-24 11:53:14 -0400

    [MAINT] Modernize type annotation (#1107)
    Modernize type annotation style

--- nipoppy/config/pipeline.py
+++ nipoppy/config/pipeline.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import functools
 from abc import ABC
-from typing import Annotated, Any, Optional, Union
+from typing import Annotated, Any
 
 from pydantic import (
     AfterValidator,
@@ -57,21 +57,17 @@ class PipelineInfo(BaseModel):
 class BasePipelineConfig(_SchemaWithContainerConfig, ABC):
     """Base schema for processing/BIDS pipeline configuration."""
 
-    _expected_pipeline_type: Optional[PipelineTypeEnum] = None
+    _expected_pipeline_type: PipelineTypeEnum | None = None
 
     NAME: str = Field(description="Name of the pipeline")
     VERSION: str = Field(description="Version of the pipeline")
-    DESCRIPTION: Optional[str] = Field(
-        default=None, description="Free description field"
-    )
+    DESCRIPTION: str | None = Field(default=None, description="Free description field")
     CONTAINER_INFO: ContainerInfo = Field(
         default=ContainerInfo(),
         description="Information about the container image file",
     )
     STEPS: list[
-        Union[
-            BidsPipelineStepConfig, ProcPipelineStepConfig, ExtractionPipelineStepConfig
-        ]
+        BidsPipelineStepConfig | ProcPipelineStepConfig | ExtractionPipelineStepConfig
     ] = Field(
         default=[],
         description="List of pipeline step configurations",
@@ -85,7 +81,7 @@ class BasePipelineConfig(_SchemaWithContainerConfig, ABC):
             ' For example: {{"REQUIRED_FILE": "This file is for running the pipeline"}}'
         ),
     )
-    PIPELINE_TYPE: Optional[PipelineTypeEnum] = None
+    PIPELINE_TYPE: PipelineTypeEnum | None = None
     SCHEMA_VERSION: Annotated[
         str,
         AfterValidator(
@@ -159,9 +155,7 @@ class BasePipelineConfig(_SchemaWithContainerConfig, ABC):
 
         return self
 
-    def get_step_config(
-        self, step_name: Optional[str] = None
-    ) -> BasePipelineStepConfig:
+    def get_step_config(self, step_name: str | None = None) -> BasePipelineStepConfig:
         """
         Return the configuration for the given step.
 
--- nipoppy/workflows/pipeline_store/search.py
+++ nipoppy/workflows/pipeline_store/search.py
@@ -1,7 +1,5 @@
 """Workflow for pipeline search command."""
 
-from typing import Optional
-
 import pandas as pd
 from rich import box
 from rich.table import Table
@@ -42,7 +40,7 @@ class PipelineSearchWorkflow(BaseWorkflow):
     def __init__(
         self,
         query: str,
-        zenodo_api: Optional[ZenodoAPI] = None,
+        zenodo_api: ZenodoAPI | None = None,
         community: bool = False,
         size: int = 10,
         verbose: bool = False,
