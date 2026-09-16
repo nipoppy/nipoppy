1b05ec36f2e2e1eab50c45979cf28523cfa9e899
mathdugre <16450132+mathdugre@users.noreply.github.com>
2026-09-16 11:13:47 -0400

    Fix the exception handler for system exit code 0.
    - Before the 0 or X would always resolve to X due to boolean resolution.
    - Also removed the logging for system exit as it only printed the exit code number
    without context. Not super useful.

--- nipoppy/cli/__init__.py
+++ nipoppy/cli/__init__.py
@@ -30,8 +30,8 @@ def exception_handler(workflow):
             "Troubleshooting: Review your configuration fields and value types."
         )
     except SystemExit as e:
-        workflow.return_code = e.code or ReturnCode.UNKNOWN_FAILURE
-        logger.error(e)
+        workflow.return_code = ReturnCode.UNKNOWN_FAILURE if e.code is None else e.code
+
     except Exception:
         workflow.return_code = ReturnCode.UNKNOWN_FAILURE
         logger.exception("Unexpected error occurred")
