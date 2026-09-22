026723521c12e5c40df349b712671a7fb6ff90e5
Mathieu Dugré <16450132+mathdugre@users.noreply.github.com>
2026-09-22 15:56:09 -0400

    [MAINT] Use fileops module across the code base. (#1148)
    * Use fileops module across the code base.
    
    * Clarify why inline statement were used: circular import

--- nipoppy/utils/utils.py
+++ nipoppy/utils/utils.py
@@ -126,10 +126,13 @@ def save_json(obj: dict, fpath: StrOrPathLike, **kwargs):
     **kwargs :
         Keyword arguments to pass to json.dump
     """
+    # inline to prevent circular import
+    from nipoppy.utils import fileops
+
     if "indent" not in kwargs:
         kwargs["indent"] = 4
     fpath = Path(fpath)
-    fpath.parent.mkdir(parents=True, exist_ok=True)
+    fileops.mkdir(fpath.parent)
     with open(fpath, "w") as file:
         json.dump(obj, file, **kwargs)
 
@@ -177,6 +180,8 @@ def save_df_with_backup(
     Path
         The path to the backup file
     """
+    from nipoppy.utils import fileops
+
     if "index" not in kwargs:
         kwargs["index"] = False
     if "sep" not in kwargs:
@@ -197,7 +202,7 @@ def save_df_with_backup(
     fpath_backup_full: Path = fpath_symlink.parent / dname_backups / fname_backup
 
     if not dry_run:
-        fpath_backup_full.parent.mkdir(parents=True, exist_ok=True)
+        fileops.mkdir(fpath_backup_full.parent)
         df.to_csv(fpath_backup_full, **kwargs)
 
         if use_relative_path:
@@ -209,7 +214,7 @@ def save_df_with_backup(
 
         if fpath_symlink.is_symlink() or fpath_symlink.exists():
             fpath_symlink.unlink()
-        fpath_symlink.symlink_to(fpath_backup_to_link)
+        fileops.symlink(source=fpath_backup_to_link, target=fpath_symlink)
 
     return Path(fpath_backup_full)
 
